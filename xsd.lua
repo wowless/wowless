504111e4e6a91bc9b28d6a0c4e7794bc19f3f089
Ferronn <ferronn@ferronn.dev>
2021-06-14 18:49:11 -0500

    identifying top-level xsd tags

--- xsd.lua
+++ xsd.lua
@@ -1,7 +1,17 @@
 local handler = require('xmlhandler.dom')
 local xml2lua = require('xml2lua')
 
-local function topLevel()
+local UNIMPLEMENTED = function() end
+
+local function topLevel(e)
+  assert(e._name and e._name:sub(1, 3) == 'xs:', 'malformed tag ' .. tostring(e._name))
+  local tag = e._name:sub(4)
+  local tags = {
+    complexType = UNIMPLEMENTED,
+    element = UNIMPLEMENTED,
+    simpleType = UNIMPLEMENTED,
+  }
+  assert(tags[tag], 'unknown tag ' .. tag)
   return 'k', 'v'
 end
 
@@ -10,13 +20,21 @@ local function loadXmlString(str)
   h.options.commentNode = false
   xml2lua.parser(h):parse(str)
   assert(h.root, 'missing root')
-  assert(h.root._name == 'schema', 'wrong root')
+  assert(h.root._name == 'xs:schema', 'wrong root')
   local result = {}
   for _, kid in ipairs(h.root._children) do
     local k, v = topLevel(kid)
     result[k] = v
   end
   return result
+end
+
+local filename = ...
+if filename then
+  local file = assert(require('io').open(filename, 'r'))
+  local data = file:read('*all')
+  file:close()
+  loadXmlString(data)
 end
 
 return {
