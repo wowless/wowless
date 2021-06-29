f2a7058a65aa64e8fbd90c21d53ea8c28494d7e6
Ferronn <ferronn@ferronn.dev>
2021-06-29 17:36:17 +0000

    send events in try.sh

--- bin/try.sh
+++ bin/try.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 loglevel=${1-100}
 eval $($(dirname $0)/../.lua/bin/luarocks path)
-lua -e "require 'wowless.loader'.run($loglevel)"
+lua <<EOF
+local api = require('wowless.loader').run($loglevel)
+api.SendEvent('PLAYER_LOGIN')
+api.SendEvent('PLAYER_ENTERING_WORLD')
+EOF
