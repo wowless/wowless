d250792f3d706b94392dae063b28a63a98390bfd
Ferronn <ferronn@ferronn.dev>
2021-06-30 17:16:15 -0500

    start frame_spec

---
+++ spec/frame_spec.lua
@@ -0,0 +1,74 @@
+local api = require('wowless.api').new(function() end)
+require('wowless.env').init(api)
+
+describe('frame #small', function()
+  it('has expected defaults', function()
+    local f = api.env.CreateFrame('frame')
+    local expected = {
+      height = 0,
+      numPoints = 0,
+      shown = true,
+      size = {0, 0},
+      visible = true,
+      width = 0,
+    }
+    local actual = {
+      height = f:GetHeight(),
+      name = f:GetName(),
+      numPoints = f:GetNumPoints(),
+      parent = f:GetParent(),
+      shown = f:IsShown(),
+      size = {f:GetSize()},
+      visible = f:IsVisible(),
+      width = f:GetWidth(),
+    }
+    assert.same(expected, actual)
+  end)
+
+  it('is added to global environment if named', function()
+    local f = api.env.CreateFrame('frame', 'sillyName')
+    assert.equals(f, api.env.sillyName)
+  end)
+
+  it('handles show/hide with no parent and no kids', function()
+    local f = api.env.CreateFrame('frame')
+    assert.True(f:IsShown())
+    assert.True(f:IsVisible())
+    f:Show()
+    assert.True(f:IsShown())
+    assert.True(f:IsVisible())
+    f:Hide()
+    assert.False(f:IsShown())
+    assert.False(f:IsVisible())
+    f:Hide()
+    assert.False(f:IsShown())
+    assert.False(f:IsVisible())
+    f:Show()
+    assert.True(f:IsShown())
+    assert.True(f:IsVisible())
+  end)
+
+  it('handles show/hide across three generations', function()
+    local a = api.env.CreateFrame('frame')
+    local b = api.env.CreateFrame('frame', nil, a)
+    local c = api.env.CreateFrame('frame', nil, b)
+    local function state()
+      return {a:IsShown(), a:IsVisible(), b:IsShown(), b:IsVisible(), c:IsShown(), c:IsVisible()}
+    end
+    assert.same({true, true, true, true, true, true}, state())
+    b:Hide()
+    assert.same({true, true, false, false, true, false}, state())
+    c:Show()
+    assert.same({true, true, false, false, true, false}, state())
+    c:Hide()
+    assert.same({true, true, false, false, false, false}, state())
+    a:Show()
+    assert.same({true, true, false, false, false, false}, state())
+    b:Show()
+    assert.same({true, true, true, true, false, false}, state())
+    c:Show()
+    assert.same({true, true, true, true, true, true}, state())
+    a:Hide()
+    assert.same({false, false, true, false, true, false}, state())
+  end)
+end)
