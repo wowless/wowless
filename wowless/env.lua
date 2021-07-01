39fc71854a439c97d52da5731962e39d04e188b7
Ferronn <ferronn@ferronn.dev>
2021-07-01 16:22:26 +0000

    support checkbutton checking

--- wowless/env.lua
+++ wowless/env.lua
@@ -130,16 +130,24 @@ local function mkBaseUIObjectTypes(api)
       name = 'Button',
     },
     checkbutton = {
+      constructor = function(self, xmlattr)
+        u(self).checked = not not xmlattr.checked
+      end,
       inherits = {'button'},
       intrinsic = true,
       mixin = {
+        GetChecked = function(self)
+          return u(self).checked
+        end,
         GetCheckedTexture = function(self)
           return u(self).checkedTexture
         end,
         GetDisabledCheckedTexture = function(self)
           return u(self).disabledCheckedTexture
         end,
-        SetChecked = UNIMPLEMENTED,
+        SetChecked = function(self, checked)
+          u(self).checked = not not checked
+        end,
         SetCheckedTexture = function(self, tex)
           u(self).checkedTexture = toTexture(self, tex)
         end,
