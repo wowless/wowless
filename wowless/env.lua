665478381aa9549d141919392faa515c53354b62
Ferronn <ferronn@ferronn.dev>
2021-07-01 14:51:00 +0000

    hookscript implementation

--- wowless/env.lua
+++ wowless/env.lua
@@ -437,6 +437,23 @@ local function mkBaseUIObjectTypes(api)
         GetScript = function(self, name, bindingType)
           return u(self).scripts[bindingType or 1][string.lower(name)]
         end,
+        HookScript = function(self, name, script, bindingType)
+          local btype = bindingType or 1
+          local lname = string.lower(name)
+          local scripts = u(self).scripts[btype]
+          local old = scripts[lname]
+          if not old and btype ~= 1 then
+            api.log(1, 'cannot hook nonexistent intrinsic precall/postcall')
+            return false
+          end
+          scripts[lname] = function(...)
+            if old then
+              old(...)
+            end
+            script(...)
+          end
+          return true
+        end,
         SetScript = function(self, name, script)
           api.SetScript(self, name, 1, script)
         end,
