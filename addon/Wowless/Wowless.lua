17a048e83af0a2efb1a80e443a2ccc8cd24d3f8d
Ferronn <ferronn@ferronn.dev>
2021-07-03 17:36:46 -0500

    add more to the addon

--- addon/Wowless/Wowless.lua
+++ addon/Wowless/Wowless.lua
@@ -1,7 +1,10 @@
+---@diagnostic disable-next-line: undefined-field
+local CreateFrame = _G.CreateFrame
+
 local log = (function()
   local theLog = {}
   do
-    local frame = _G.CreateFrame('Frame')
+    local frame = CreateFrame('Frame')
     frame:RegisterEvent('PLAYER_LOGOUT')
     frame:SetScript('OnEvent', function()
       _G.WowlessLog = theLog
@@ -15,7 +18,12 @@ end)()
 do
   local function scriptHandler(name, self, ...)
     local rest = table.concat({...}, ',')
-    log('%s(%s%s)', name, self:GetName(), rest ~= '' and (',' .. rest) or '')
+    local argstr = table.concat({
+      self:GetName(),
+      self:GetParent() and tostring(self:GetParent():GetName()) or 'none',
+      rest ~= '' and rest or nil,
+    }, ',')
+    log('%s(%s)', name, argstr)
   end
   local names = {
     'OnAttributeChanged',
@@ -28,4 +36,14 @@ do
       scriptHandler(name, ...)
     end
   end
+end
+
+do
+  local frame = CreateFrame('Frame')
+  frame:RegisterEvent('PLAYER_LOGIN')
+  frame:SetScript('OnEvent', function()
+    log('before WowlessLuaFrame')
+    CreateFrame('Frame', 'WowlessLuaFrame', nil, 'WowlessLogger')
+    log('after WowlessLuaFrame')
+  end)
 end
