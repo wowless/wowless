485908e00869ca3cab589583cab2144f8553b017
Ferronn <ferronn@ferronn.dev>
2021-07-06 10:16:17 -0500

    simple xml test

--- spec/loader_spec.lua
+++ spec/loader_spec.lua
@@ -1,5 +1,6 @@
 describe('loader #small', function()
   local api = require('wowless.api').new(function() end)
+  require('wowless.env').init(api)
   local loader = require('wowless.loader').loader(api)
 
   local function loadXml(str)
@@ -9,5 +10,20 @@ describe('loader #small', function()
   it('loads empty xml', function()
     loadXml('<Ui/>')
     assert.same(0, api.GetErrorCount())
+  end)
+
+  it('creates simple frames', function()
+    loadXml([[
+      <Ui>
+        <Frame name='Frame1' />
+        <Frame name='Frame2' />
+      </Ui>
+    ]])
+    assert.same(0, api.GetErrorCount())
+    local frame1 = api.env.Frame1
+    local frame2 = api.env.Frame2
+    assert.Not.Nil(frame1)
+    assert.Not.Nil(frame2)
+    assert.Not.equal(frame1, frame2)
   end)
 end)
