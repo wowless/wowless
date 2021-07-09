7017dbd1c4c897635d8a1da7134050ef260b155c
Ferronn <ferronn@ferronn.dev>
2021-07-09 21:21:31 +0000

    fix toc key/value parsing again

--- wowless/loader.lua
+++ wowless/loader.lua
@@ -407,7 +407,7 @@ local function loader(api)
     for line in io.lines(tocFile) do
       line = line:match('^%s*(.-)%s*$')
       if line:sub(1, 3) == '## ' then
-        local key, value = line:match('([%w_]+): ([%w_]+)', 4)
+        local key, value = line:match('([^:]+): (.*)', 4)
         if key then
           attrs[key] = value
         end
