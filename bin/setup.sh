11161b9c2a51034a73d3b0496c440ce218aa8d04
Ferronn <ferronn@ferronn.dev>
2021-07-12 14:44:08 +0000

    specify pythonpath on local hererocks

--- bin/setup.sh
+++ bin/setup.sh
@@ -2,7 +2,7 @@
 set -e
 DIR="$PWD/.lua"
 python3 -m pip install -t "$DIR" hererocks
-"$DIR"/bin/hererocks -l 5.1 -r 3.5.0 "$DIR"
+PYTHONPATH="$DIR" "$DIR"/bin/hererocks -l 5.1 -r 3.5.0 "$DIR"
 (cd tainted-lua && make linux && make install INSTALL_TOP="$DIR" && make clean)
 eval $("$DIR"/bin/luarocks path)
 luarocks install luacheck
