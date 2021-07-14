52685d7cc7cff95b07355a648a1065499eae2217
Ferronn <ferronn@ferronn.dev>
2021-07-14 16:17:33 +0000

    fetch all three versions

--- bin/setup.sh
+++ bin/setup.sh
@@ -5,4 +5,6 @@ PYTHONPATH=.lua .lua/bin/hererocks -l 5.1 -r 3.5.0 .lua
 eval $(.lua/bin/luarocks path)
 .lua/bin/luarocks install luacheck
 .lua/bin/luarocks build --only-deps
+.lua/bin/wowcig -p wow
 .lua/bin/wowcig -p wow_classic
+.lua/bin/wowcig -p wow_classic_era
