0572d3cfd9819b7a25dc581979be099d622f7845
Ferronn <ferronn@ferronn.dev>
2021-09-07 13:51:33 +0000

    attempt to do runner tests in parallel

--- spec/runner_spec.lua
+++ spec/runner_spec.lua
@@ -1,18 +1,23 @@
 describe('runner', function()
-  local versions = {
-    wow_classic = 'TBC',
-    wow_classic_era = 'Vanilla',
-    wow_classic_era_ptr = 'Vanilla',
-    wow_classic_ptr = 'TBC',
-  }
-  local runner = require('wowless.runner')
-  for k, v in pairs(versions) do
-    it('loads ' .. k, function()
-      local api = runner.run({
-        dir = 'extracts/' .. k .. '/Interface',
-        version = v
-      })
-      assert.same(0, api.GetErrorCount())
-    end)
-  end
+  it('loads required versions', function()
+    local versions = {
+      wow_classic = 'TBC',
+      wow_classic_era = 'Vanilla',
+      wow_classic_era_ptr = 'Vanilla',
+      wow_classic_ptr = 'TBC',
+    }
+    local cmds = {'set -e ;'}
+    for k, v in pairs(versions) do
+      table.insert(cmds, string.format('bin/run.sh 0 %s %s &', k, v))
+    end
+    for _ = 2, #cmds do
+      table.insert(cmds, 'wait -n ;')
+    end
+    local inner = string.format('bash -c "%s"', table.concat(cmds, ''))
+    local outer = string.format('bash -c \'%s\' >/dev/null; echo $?', inner)
+    local handle = io.popen(outer)
+    local content = handle:read('*all')
+    handle:close()
+    assert.same('0\n', content)
+  end)
 end)
--- wowless.lua
+++ wowless.lua
@@ -1,6 +1,11 @@
-require('wowless.runner').run({
+local tag = arg[2] or 'wow_classic'
+local api = require('wowless.runner').run({
   loglevel = arg[1] and tonumber(arg[1]) or 0,
-  dir = 'extracts/' .. (arg[2] or 'wow_classic') .. '/Interface',
+  dir = 'extracts/' .. tag .. '/Interface',
   version = arg[3] or 'TBC',
   otherAddonDirs = { arg[4] },
 })
+if api.GetErrorCount() ~= 0 then
+  io.stderr:write('failure on ' .. tag .. '\n')
+  os.exit(1)
+end
