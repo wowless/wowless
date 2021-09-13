bfe9bbf88cd772368f2ed878383c7bd17b3ecd1b
Ferronn <ferronn@ferronn.dev>
2021-09-13 16:15:37 -0500

    ensure that name is set properly

--- spec/wowapi_spec.lua
+++ spec/wowapi_spec.lua
@@ -1,8 +1,12 @@
 describe('wowapi #small', function()
   for f in require('lfs').dir('wowapi') do
     if f:sub(-4) == '.lua' then
-      describe(f:sub(1, -5), function()
+      local fn = f:sub(1, -5)
+      describe(fn, function()
         local t = dofile('wowapi/' .. f)
+        it('has the right name', function()
+          assert.same(fn, t.name)
+        end)
         local impl = t.impl
         for _, test in ipairs(t.tests) do
           (test.pending and pending or it)(test.name, function()
