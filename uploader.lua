e528818c1a202f63ba5a2d9e36823f76f670d5e7
Ferronn <ferronn@ferronn.dev>
2021-09-25 09:55:28 -0500

    quote infile in uploader

--- uploader.lua
+++ uploader.lua
@@ -5,9 +5,5 @@ local buildinfo = (function()
   return env.TheFlatDumperBuildInfo
 end)()
 local outfile = string.format('%s.%s.lua', buildinfo[1], buildinfo[2])
-assert(os.execute(table.concat({
-  'gsutil',
-  'cp',
-  infile,
-  'gs://wow.ferronn.dev/gscrapes/' .. outfile,
-}, ' ')))
+local outurl = 'gs://wow.ferronn.dev/gscrapes/' .. outfile
+assert(os.execute(string.format('gsutil cp %q %s', infile, outurl)))
