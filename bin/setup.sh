c0f86a2803aa2924e82c7d1ace8bff803e6f41a4
Ferronn <ferronn@ferronn.dev>
2021-10-09 21:05:44 -0500

    only gscrape current versions in setup

--- bin/setup.sh
+++ bin/setup.sh
@@ -5,26 +5,17 @@ PYTHONPATH=.lua .lua/bin/hererocks -l 5.1 -r 3.5.0 .lua
 eval $(.lua/bin/luarocks path)
 .lua/bin/luarocks install luacheck
 .lua/bin/luarocks build --only-deps
-if [ "$1" != "skipcig" ]
-then
-  rm -f extracts/wow*
-  .lua/bin/wowcig -p wow
-  .lua/bin/wowcig -p wowt
-  .lua/bin/wowcig -p wow_classic
-  .lua/bin/wowcig -p wow_classic_era
-  .lua/bin/wowcig -p wow_classic_era_ptr
-  .lua/bin/wowcig -p wow_classic_ptr
-fi
-for d in extracts/*
-do
-  if [ ! -h $d ]
+for p in wow wowt wow_classic wow_classic_era wow_classic_era_ptr wow_classic_ptr; do
+  d="extracts/$p"
+  if [ "$1" != "skipcig" ]; then
+    rm -f "$d"
+    .lua/bin/wowcig -p "$p"
+  fi
+  v=$(basename $(readlink "$d"))
+  if ! bash -c "curl -s http://storage.googleapis.com/wow.ferronn.dev/gscrapes/$v.lua | \
+       .lua/bin/lua gextract.lua /dev/stdin 2>/dev/null > \
+       $d/Interface/GlobalEnvironment.lua"
   then
-    v=$(basename $d)
-    if ! bash -c "curl -s http://storage.googleapis.com/wow.ferronn.dev/gscrapes/$v.lua | \
-         .lua/bin/lua gextract.lua /dev/stdin 2>/dev/null > \
-         extracts/$v/Interface/GlobalEnvironment.lua"
-    then
-      echo "failed to extract globals for $v"
-    fi
+    echo "failed to extract globals for $p"
   fi
 done
