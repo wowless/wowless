7183c95447e9bc4ad1eaf0649cdb8eae1faba685
Ferronn <ferronn@ferronn.dev>
2021-10-29 15:54:56 +0000

    bump wowapi, C_CVar moved there

--- wowless/env.lua
+++ wowless/env.lua
@@ -1309,39 +1309,6 @@ local function mkWowEnv(api, loader)
         return Mixin({r=0, g=0, b=0}, api.env.ColorMixin)
       end,
     },
-    C_CVar = (function()
-      local cvarDefaults = {
-        cameraSmoothStyle = '0',
-        cameraSmoothTrackingStyle = '0',
-        NamePlateClassificationScale = '1',
-        NamePlateHorizontalScale = '1',
-        nameplateMotion = '0',
-        NamePlateVerticalScale = '1',
-        remoteTextToSpeechVoice = '1',
-        timeMgrAlarmTime = '0',
-      }
-      local cvars = {}
-      local function GetCVar(var)
-        return cvars[var] or cvarDefaults[var]
-      end
-      return {
-        GetCVar = GetCVar,
-        GetCVarBitfield = UNIMPLEMENTED,
-        GetCVarBool = function(var)
-          return GetCVar(var) == "1"
-        end,
-        GetCVarDefault = function(var)
-          return cvarDefaults[var]
-        end,
-        RegisterCVar = function(var, value)
-          cvars[var] = value
-        end,
-        SetCVar = function(var, value)
-          cvars[var] = value
-        end,
-        SetCVarBitfield = UNIMPLEMENTED,
-      }
-    end)(),
     C_DateAndTime = {
       GetCurrentCalendarTime = function()
         return {
