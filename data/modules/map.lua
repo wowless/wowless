69bde54621acd58c227eeb784302c587b4de0194
Ferronn <ferronn@ferronn.dev>
2021-11-02 19:18:12 -0500

    use env.Vector2DMixin in GetUserWaypointPositionForMap

--- data/modules/map.lua
+++ data/modules/map.lua
@@ -1,3 +1,5 @@
+local env = ...
+
 return {
   api = {
     GetMapInfo = function(uiMapID)
@@ -10,32 +12,7 @@ return {
       }
     end,
     GetUserWaypointPositionForMap = function()
-      local function Vector2D_Dot(leftX, leftY, rightX, rightY)
-        return leftX * rightX + leftY * rightY;
-      end
-      local function Vector2D_GetLengthSquared(x, y)
-        return Vector2D_Dot(x, y, x, y);
-      end
-      local function Vector2D_GetLength(x, y)
-        return math.sqrt(Vector2D_GetLengthSquared(x, y));
-      end
-      return {
-        Add = function() end,
-        Clone = function(self) return self; end,
-        Cross = function() end,
-        DivideBy = function() end,
-        Dot = function(self,other) return Vector2D_Dot(self.x, self.y, other:GetXY()); end,
-        GetLength = function(self) return Vector2D_GetLength(self:GetXY()); end,
-        GetLengthSquared = function(self) return Vector2D_GetLengthSquared(self:GetXY()); end,
-        GetXY = function() return 1,1 end,
-        IsEqualTo = function()return true end,
-        IsZero = function() return false end,
-        Normalize = function()  end,
-        RotateDirection = function() end,
-        ScaleBy = function() end,
-        SetXY = function() return true end,
-        Subtract = function() end,
-      }
+      return env.Mixin({x=1, y=1}, env.Vector2DMixin)
     end,
   },
 }
