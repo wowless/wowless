5fbb619b0395504ec52c0872fd1968ae3832af6c
Ferronn <ferronn@ferronn.dev>
2021-11-08 17:08:55 +0000

    hacky script to run tests on addons

---
+++ bin/evaladdons.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+eval $(.lua/bin/luarocks path)
+.lua/bin/lua tools/curseforge.lua 13501
+function dotest() {
+  local flavor=$1
+  local build=$2
+  local ptr=$3
+  for d in extracts/addons/$flavor/*; do
+    if [ -d $d ]; then
+      echo Testing $flavor $(basename $d)
+      bin/run.sh 0 $build $flavor $d
+      echo Testing $flavor $(basename $d) \(PTR\)
+      bin/run.sh 0 $ptr $flavor $d
+    fi
+  done
+}
+dotest Vanilla wow_classic_era wow_classic_era_ptr
+dotest TBC wow_classic wow_classic_ptr
+dotest Mainline wow wowt
