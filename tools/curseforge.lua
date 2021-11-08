3e7b308520eee5fb41eb899e2de54fc210a1d7f6
Ferronn <ferronn@ferronn.dev>
2021-11-08 16:36:02 +0000

    associate cf flavors with wow names for them

--- tools/curseforge.lua
+++ tools/curseforge.lua
@@ -1,19 +1,25 @@
 local http_request = require('http.request').new_from_uri
 local json = require('JSON')
 
+local flavors = {
+  wow_burning_crusade = 'TBC',
+  wow_classic = 'Vanilla',
+  wow_retail = 'Mainline',
+}
+
 local function main(cfid)
   local url = 'https://addons-ecs.forgesvc.net/api/v2/addon/' .. cfid
   local headers, stream = assert(http_request(url):go())
   assert(headers:get(":status") == "200")
   local cfg = json:decode(assert(stream:get_body_as_string()))
   table.sort(cfg.latestFiles, function(a, b) return a.id > b.id end)
-  for _, flavor in ipairs({'wow_classic', 'wow_burning_crusade', 'wow_retail'}) do
+  for cfFlavor, wowFlavor in pairs(flavors) do
     for _, file in ipairs(cfg.latestFiles) do
-      if file.gameVersionFlavor == flavor and
+      if file.gameVersionFlavor == cfFlavor and
           file.releaseType == 1 and
           not file.displayName:match('-nolib') and
           not file.isAlternate then
-        print(flavor, file.downloadUrl)
+        print(wowFlavor, file.downloadUrl)
         break
       end
     end
