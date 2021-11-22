a5adc3e08207201081e6b48b4247d5f41af0c27f
Ferronn <ferronn@ferronn.dev>
2021-11-22 21:28:30 +0000

    move schema validation to wowapi

---
+++ wowapi/schema.lua
@@ -0,0 +1,37 @@
+local function validate(schematype, v)
+  if schematype == 'any' then
+    return
+  elseif schematype == 'string' then
+    assert(type(v) == 'string', 'expected string')
+  elseif type(schematype) ~= 'table' then
+    error('expected type table')
+  elseif schematype.record then
+    assert(type(v) == 'table', 'expected table')
+    -- TODO required/optional fields
+    for k2, v2 in pairs(v) do
+      validate(assert(schematype.record[k2], 'unknown field ' .. k2), v2)
+    end
+  elseif schematype.mapof then
+    assert(type(v) == 'table', 'expected table')
+    for _, v2 in pairs(v) do
+      validate(schematype.mapof, v2)
+    end
+  elseif schematype.sequenceof then
+    assert(type(v) == 'table', 'expected table')
+    local max = 0
+    for k2, v2 in pairs(v) do
+      assert(type(k2) == 'number', 'expected number key')
+      max = k2 > max and k2 or max
+      validate(schematype.sequenceof, v2)
+    end
+    assert(max == #v, 'expected array')
+  elseif schematype.oneof then
+    error('unsupported')
+  else
+    error('expected record/mapof/sequenceof/oneof')
+  end
+end
+
+return {
+  validate = validate,
+}
