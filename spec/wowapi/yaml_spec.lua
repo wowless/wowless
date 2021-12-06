141ab637872a36db6462b5fa2dae1359c7a3438f
Ferronn <ferronn@ferronn.dev>
2021-12-06 16:48:08 +0000

    full parse test, including null

--- spec/wowapi/yaml_spec.lua
+++ spec/wowapi/yaml_spec.lua
@@ -1,8 +1,36 @@
 describe('yaml', function()
   local yaml = require('wowapi.yaml')
+  local null = require('lyaml').null
   describe('parse', function()
     it('works on empty', function()
       assert.Nil(yaml.parse(''))
+    end)
+    it('works on all types', function()
+      local input = [[
+---
+bool: true
+number: 42
+string: foo
+empty_string: ''
+nullkey: null
+sequence:
+- 99
+- null
+- bar
+record:
+  baz: quux
+  frob: nicate
+]]
+      local expected = {
+        bool = true,
+        number = 42,
+        string = 'foo',
+        empty_string = '',
+        nullkey = null,
+        sequence = { 99, null, 'bar' },
+        record = { baz = 'quux', frob = 'nicate' },
+      }
+      assert.same(expected, yaml.parse(input))
     end)
   end)
   describe('pprint', function()
