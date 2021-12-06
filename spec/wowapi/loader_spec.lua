3e3d9bca65ee03550bf746a520a5adbb12a54ded
Ferronn <ferronn@ferronn.dev>
2021-12-06 19:02:04 +0000

    remove now unused stub/returns, fixes #75

--- spec/wowapi/loader_spec.lua
+++ spec/wowapi/loader_spec.lua
@@ -6,21 +6,4 @@ describe('loader', function()
       assert.same('table', type(loader.loadFunctions(v, {}, nil, {})))
     end)
   end
-
-  describe('getFn', function()
-    it('works with null stub results', function()
-      local api = require('wowapi.yaml').parse([[
----
-name: Foo
-status: stub
-returns:
-  - null
-  - 42
-  - null
-]])
-      local modules = {}
-      local fn = loader.getFn(api, modules)
-      assert.same({nil, 42, nil}, {fn()})
-    end)
-  end)
 end)
