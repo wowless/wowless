9964fae5cea89f8e8f4cbc6af5dafa8ac4de23e5
Ferronn <ferronn@ferronn.dev>
2021-12-07 16:06:06 +0000

    fix missing return in xpcall

--- data/impl/xpcall.lua
+++ data/impl/xpcall.lua
@@ -1,4 +1,4 @@
 local f, e = ...
 local args = {select(3, ...)}
 local n = select('#', ...) - 2
-return xpcall(function() f(unpack(args, 1, n)) end, e)
+return xpcall(function() return f(unpack(args, 1, n)) end, e)
