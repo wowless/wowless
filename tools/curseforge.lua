d93e885ad86fdd9784d7aa11678f7849bc004296
Ferronn <ferronn@ferronn.dev>
2021-12-25 17:24:53 -0600

    change curseforge.lua to use game version type id instead of flavor

--- tools/curseforge.lua
+++ tools/curseforge.lua
@@ -1,27 +1,32 @@
 local json = require('JSON')
 
 local flavors = {
-  wow_burning_crusade = 'TBC',
-  wow_classic = 'Vanilla',
-  wow_retail = 'Mainline',
+  Mainline = 517,   -- wow_retail
+  TBC = 73246,      -- wow_burning_crusade
+  Vanilla = 67408,  -- wow_classic
 }
 
 local function main(cfid)
   local pipe = io.popen('wget -qO- https://addons-ecs.forgesvc.net/api/v2/addon/' .. cfid)
   local cfg = json:decode(pipe:read('*all'))
   pipe:close()
-  table.sort(cfg.latestFiles, function(a, b)
-    if a.releaseType < b.releaseType then
+  local latestFilesMap = {}
+  for _, f in ipairs(cfg.latestFiles) do
+    latestFilesMap[f.id] = f
+  end
+  table.sort(cfg.gameVersionLatestFiles, function(a, b)
+    if a.fileType < b.fileType then
       return true
-    elseif a.releaseType > b.releaseType then
+    elseif a.fileType > b.fileType then
       return false
     else
-      return a.id > b.id
+      return a.projectFileId > b.projectFileId
     end
   end)
-  for cfFlavor, wowFlavor in pairs(flavors) do
-    for _, file in ipairs(cfg.latestFiles) do
-      if file.gameVersionFlavor == cfFlavor and
+  for wowFlavor, cfFlavor in pairs(flavors) do
+    for _, gvFile in ipairs(cfg.gameVersionLatestFiles) do
+      local file = latestFilesMap[gvFile.projectFileId]
+      if gvFile.gameVersionTypeId == cfFlavor and
           not file.displayName:match('-nolib') and
           not file.isAlternate then
         os.execute(([[
