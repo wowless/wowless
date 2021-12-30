f8f34d89711c4d6ceb8f8c9fbf232f2cb30391b9
Ferronn <ferronn@ferronn.dev>
2021-12-29 21:35:04 -0600

    use lua 5.1.4

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 python3 -m pip install -t .lua hererocks
-PYTHONPATH=.lua .lua/bin/hererocks -l 5.1 -r 3.5.0 .lua
+PYTHONPATH=.lua .lua/bin/hererocks -l 5.1.4 -r 3.5.0 .lua
 eval $(.lua/bin/luarocks path)
 .lua/bin/luarocks install luacheck
 .lua/bin/luarocks build --only-deps
