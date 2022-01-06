e5a4f61c7a4c3ba31dd6087d9e56ffbc8c9648ae
Ferronn <ferronn@ferronn.dev>
2022-01-06 22:20:16 +0000

    clean up bin/extract.sh

--- bin/extract.sh
+++ bin/extract.sh
@@ -3,9 +3,11 @@ set -e
 eval "$(.lua/bin/luarocks path)"
 p="$1"
 v="$(gsutil cat gs://wowless.dev/extracts/"$p".txt)"
+echo -en "Extracting $v..."
 mkdir -p extracts
-gsutil cp gs://wowless.dev/extracts/"$v".zip extracts/
-unzip extracts/"$v".zip -d extracts
-gsutil cp gs://wowless.dev/gscrapes/"$v".lua extracts/
+gsutil -q cp gs://wowless.dev/extracts/"$v".zip extracts/
+unzip -d extracts -q -u extracts/"$v".zip
+gsutil -q cp gs://wowless.dev/gscrapes/"$v".lua extracts/
 .lua/bin/lua tools/gextract.lua extracts/"$v".lua > extracts/"$v"/Interface/GlobalEnvironment.lua
 ln -sf "$v" extracts/"$p"
+echo " done."
