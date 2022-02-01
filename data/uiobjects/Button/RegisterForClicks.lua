3447434758c446170512e60626bd290c73129613
Ferronn <ferronn@ferronn.dev>
2022-02-01 19:34:36 +0000

    sanity check RegisterForClicks arguments

--- data/uiobjects/Button/RegisterForClicks.lua
+++ data/uiobjects/Button/RegisterForClicks.lua
@@ -1,7 +1,27 @@
+local validValues = (function()
+  local buttons = {
+    'Any',
+    'LeftButton',
+    'RightButton',
+    'MiddleButton',
+    'Button4',
+    'Button5',
+  }
+  local directions = { 'Up', 'Down' }
+  local v = {}
+  for _, b in ipairs(buttons) do
+    for _, d in ipairs(directions) do
+      v[b .. d] = true
+    end
+  end
+  return v
+end)()
 return (function(self, ...)
   local ud = u(self)
   util.twipe(ud.registeredClicks)
-  for _, type in ipairs({...}) do
+  for i = 1, select('#', ...) do
+    local type = select(i, ...)
+    assert(validValues[type], 'invalid click registration type')
     ud.registeredClicks[type] = true
   end
 end)(...)
