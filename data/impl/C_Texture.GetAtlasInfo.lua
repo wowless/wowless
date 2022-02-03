03b013c277252627a42741f63b87043c6a144331
Ferronn <ferronn@ferronn.dev>
2022-02-03 21:00:59 +0000

    fixes suggested by meo

--- data/impl/C_Texture.GetAtlasInfo.lua
+++ data/impl/C_Texture.GetAtlasInfo.lua
@@ -7,15 +7,15 @@ for m in uitextureatlasmember() do
     for a in uitextureatlas() do
       if a.ID == m.UiTextureAtlasID then
         return {
-          bottomTexCoord = m.CommittedBottom,
+          bottomTexCoord = m.CommittedBottom / a.AtlasHeight,
           file = a.FileDataID,
-          height = a.AtlasHeight,
-          leftTexCoord = m.CommittedLeft,
-          rightTexCoord = m.CommittedRight,
-          tilesHorizontally = not not band(m.CommittedFlags, 0x4),
-          tilesVertically = not not band(m.CommittedFlags, 0x2),
-          topTexCoord = m.CommittedTop,
-          width = a.AtlasWidth,
+          height = m.OverrideHeight ~= 0 and m.OverrideHeight or m.CommittedBottom - m.CommittedTop,
+          leftTexCoord = m.CommittedLeft / a.AtlasWidth,
+          rightTexCoord = m.CommittedRight / a.AtlasWidth,
+          tilesHorizontally = band(m.CommittedFlags, 0x4) ~= 0,
+          tilesVertically = band(m.CommittedFlags, 0x2) ~= 0,
+          topTexCoord = m.CommittedTop / a.AtlasHeight,
+          width = m.OverrideWidth ~= 0 and m.OverrideWidth or m.CommittedRight - m.CommittedLeft,
         }
       end
     end
