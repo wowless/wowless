f1ead813a225b1380928da9f4e6ea7a451a52f64
Ferronn <ferronn@ferronn.dev>
2022-02-08 20:32:29 +0000

    wow_classic_era handles allevents

--- wowless/runner.lua
+++ wowless/runner.lua
@@ -75,6 +75,7 @@ local function run(cfg)
   if cfg.allevents then
     local eventBlacklist = {
       OPEN_MASTER_LOOT_LIST = true,
+      PARTY_INVITE_CANCEL = true, -- does not fire in vanilla
       PLAYER_LOGIN = true,
       PLAYER_LOGOUT = true,
       UPDATE_MASTER_LOOT_LIST = true,
