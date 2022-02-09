9b6e53dcb0dfbf26f0787f99a24baaa64d2956db
Ferronn <ferronn@ferronn.dev>
2022-02-09 16:50:29 +0000

    cmdline flags for wowless.lua, fixes #109

--- wowless.lua
+++ wowless.lua
@@ -1,17 +1,28 @@
+local productToFlavor = {
+  wow = 'Mainline',
+  wowt = 'Mainline',
+  wow_classic = 'TBC',
+  wow_classic_era = 'Vanilla',
+  wow_classic_era_ptr = 'Vanilla',
+  wow_classic_ptr = 'TBC',
+}
 local args = (function()
   local parser = require('argparse')()
+  parser:option('--loglevel', 'log level')
+  parser:option('--product', 'product tag')
+  parser:option('--addondir', 'addon directory to test')
   parser:flag('--allevents', 'send all nullary events')
-  parser:argument('loglevel', 'log level'):default('0')
-  parser:argument('product', 'product tag'):default('wow_classic')
-  parser:argument('flavor', 'product flavor'):default('TBC')
-  parser:argument('addon', 'addon directory to test'):args('?')
+  parser:argument('deprecated_loglevel', 'log level'):default('0')
+  parser:argument('deprecated_product', 'product tag'):default('wow_classic')
+  parser:argument('deprecated_flavor', 'product flavor'):default('TBC')
+  parser:argument('deprecated_addon', 'addon directory to test'):args('?')
   return parser:parse()
 end)()
 local api = require('wowless.runner').run({
-  loglevel = assert(tonumber(args.loglevel)),
-  dir = 'extracts/' .. args.product,
-  version = args.flavor,
-  otherAddonDirs = { args.addon },
+  loglevel = assert(tonumber(args.loglevel or args.deprecated_loglevel)),
+  dir = 'extracts/' .. (args.product or args.deprecated_product),
+  version = (args.product and productToFlavor[args.product] or args.deprecated_flavor),
+  otherAddonDirs = { args.addondir or args.deprecated_addon },
   allevents = args.allevents,
 })
 if api.GetErrorCount() ~= 0 then
