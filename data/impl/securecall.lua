594ead4e02e312abae9b9f0f2da0e9e8b485d5ad
Ferronn <ferronn@ferronn.dev>
2022-02-17 17:18:29 +0000

    use tainted-lua securecall, but keep env lookup in lua

--- data/impl/securecall.lua
+++ data/impl/securecall.lua
@@ -4,9 +4,4 @@ if type(func) == 'string' then
   assert(env[func], 'securecall of unknown function ' .. func)
   func = env[func]
 end
--- use tainted-lua if available
-if securecall then
-  return securecall(func, select(3, ...))
-else
-  return func(select(3, ...))
-end
+return securecall(func, select(3, ...))
