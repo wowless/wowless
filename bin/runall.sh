ab6639d4d3a2abf99cd90398e46c200265b18752
Ferronn <ferronn@ferronn.dev>
2022-02-25 02:43:11 +0000

    fix runall

--- bin/runall.sh
+++ bin/runall.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 mkdir -p out
-bin/run.sh 0 wow Mainline &> out/wow.txt &
-bin/run.sh 0 wowt Mainline &> out/wowt.txt &
-bin/run.sh 0 wow_classic TBC &> out/wow_classic.txt &
-bin/run.sh 0 wow_classic_ptr TBC &> out/wow_classic_ptr.txt &
-bin/run.sh 0 wow_classic_era Vanilla &> out/wow_classic_era.txt &
-bin/run.sh 0 wow_classic_era_ptr Vanilla &> out/wow_classic_era_ptr.txt &
-wait
+f() {
+  for p in wow wowt wow_classic wow_classic_era wow_classic_era_ptr wow_classic_ptr; do
+    bin/run.sh -p $p &> out/$p.txt &
+  done
+  wait
+}
+time f
+ls -l out
