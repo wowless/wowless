56759f0c03ef28031f7c8cd9ac8e7d813aa08020
Ferronn <ferronn@ferronn.dev>
2022-03-29 15:47:35 +0000

    closer to blp parsing

--- wowless/blp.lua
+++ wowless/blp.lua
@@ -23,6 +23,10 @@ local dxt5 = vstruct.compile([[
   colorTable: { [ 4 | 16*u2 ] }
 ]])
 
+local rgb565 = vstruct.compile([[
+  [ 2 | r: u5 g: u6 b: u5 ]
+]])
+
 local function read(filename)
   local f = assert(io.open(filename))
   local header = blpHeader:read(f)
@@ -32,13 +36,39 @@ local function read(filename)
   assert(header.alphaSize == 8)
   assert(header.pixelFormat == 7) -- DXT5
   assert(header.hasMips == 17)
+  assert(header.width % 4 == 0)
+  assert(header.height % 4 == 0)
   assert(header.mipOffsets[1] == 20 + 64 + 64 + 1024) -- header size
   assert(header.mipSizes[1] == header.width * header.height)
-  for _ = 1, header.width * header.height / 16 do
-    dxt5:read(f)
+  local rgblines = {}
+  for _ = 1, header.height / 4 do
+    local lines = { {}, {}, {}, {} }
+    for _ = 1, header.width / 4 do
+      local t = dxt5:read(f)
+      -- Ignore alpha for now, just produce rgb.
+      local c2, c3
+      if t.c0 > t.c1 then
+        c2 = t.c0 * 2 / 3 + t.c1 / 3
+        c3 = t.c0 / 3 + t.c1 * 2 / 3
+      else
+        c2 = t.c0 / 2 + t.c1 / 2
+        c3 = 0
+      end
+      for row = 1, 4 do
+        for col = 1, 4 do
+          local cx = t.colorTable[(row - 1) * 4 + col]
+          local c = cx == 0 and t.c0 or cx == 1 and t.c1 or cx == 2 and c2 or c3
+          local rgb = rgb565:read(vstruct.write('u2', '', { c }))
+          table.insert(lines[row], string.char(rgb.r * 256 / 32, rgb.g * 256 / 64, rgb.b * 256 / 32))
+        end
+      end
+    end
+    for _, line in ipairs(lines) do
+      table.insert(rgblines, table.concat(line, ''))
+    end
   end
   assert(f:close())
-  return header.width, header.height
+  return header.width, header.height, table.concat(rgblines, '')
 end
 
 return {
