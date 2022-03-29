106e4c4a2ea9161f3852112f8705836c23bbb4b6
Ferronn <ferronn@ferronn.dev>
2022-03-29 13:34:00 +0000

    split blp and png

---
+++ wowless/png.lua
@@ -0,0 +1,54 @@
+local vstruct = require('vstruct')
+local zlib = require('zlib')
+
+local function crc32(...)
+  local c = zlib.crc32()
+  for i = 1, select('#', ...) do
+    c = zlib.crc32(c, select(i, ...))
+  end
+  return c
+end
+
+local pngChunks = {
+  IDAT = vstruct.compile('s'),
+  IEND = vstruct.compile(''),
+  IHDR = vstruct.compile([[>
+    width: u4
+    height: u4
+    bitDepth: u1
+    colorType: u1
+    compressionMethod: u1
+    filterMethod: u1
+    interlaceMethod: u1
+  ]]),
+}
+
+local function writePNGChunk(f, type, data)
+  local str = pngChunks[type]:write('', data)
+  local fmt = ('> u4 s4 s%d u4'):format(#str)
+  vstruct.write(fmt, f, { #str, type, str, crc32(type, str) })
+end
+
+local function writePNG(filename, width, height, data)
+  local f = assert(io.open(filename, 'wb'))
+  assert(f:write('\137PNG\r\n\26\n'))
+  writePNGChunk(f, 'IHDR', {
+    width = width,
+    height = height,
+    bitDepth = 8,
+    colorType = 2,
+    compressionMethod = 0,
+    filterMethod = 0,
+    interlaceMethod = 0,
+  })
+  local lines = {}
+  for i = 1, height do
+    table.insert(lines, '\0')
+    table.insert(lines, data:sub((i - 1) * width * 3 + 1, i * width * 3))
+  end
+  writePNGChunk(f, 'IDAT', { zlib.compress(table.concat(lines, '')) })
+  writePNGChunk(f, 'IEND', {})
+  assert(f:close())
+end
+
+writePNG('temp.png', 100, 100, ('\0\255\0'):rep(10000))
