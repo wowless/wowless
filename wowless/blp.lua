ffbeb0d07d64d50347db2b70080759c824bd7d91
Ferronn <ferronn@ferronn.dev>
2022-03-31 02:38:40 +0000

    dxt3

--- wowless/blp.lua
+++ wowless/blp.lua
@@ -14,6 +14,13 @@ local blpHeader = vstruct.compile([[
   palette: { 256*u4 }
 ]])
 
+local dxt3 = vstruct.compile([[
+  alphaTable: { [ 8 | 16*u4 ] }
+  c0: { [ 2 | r: u5 g: u6 b: u5 ] }
+  c1: { [ 2 | r: u5 g: u6 b: u5 ] }
+  colorTable: { [ 4 | 16*u2 ] }
+]])
+
 local dxt5 = vstruct.compile([[
   a0: u1
   a1: u1
@@ -88,7 +95,7 @@ local function read(filename)
   assert(header.version == 1)
   assert(header.colorEncoding == 2) -- DXT
   assert(header.alphaSize == 8)
-  assert(header.pixelFormat == 7) -- DXT5
+  assert(header.pixelFormat == 1 or header.pixelFormat == 7) -- DXT5
   assert(header.hasMips == 1 or header.hasMips == 17)
   assert(header.width % 4 == 0)
   assert(header.height % 4 == 0)
@@ -98,15 +105,28 @@ local function read(filename)
   for _ = 1, header.height / 4 do
     local lines = { {}, {}, {}, {} }
     for _ = 1, header.width / 4 do
-      local t = dxt5:read(f)
-      local cc = dxt1rgb(t.c0, t.c1)
-      local aa = dxt5alpha(t.a0, t.a1)
-      for row = 1, 4 do
-        for col = 1, 4 do
-          local idx = 17 - ((row - 1) * 4 + col)
-          local a = aa[t.alphaTable[idx]]
-          local rgb = cc[t.colorTable[idx]]
-          table.insert(lines[row], string.char(rgb.r, rgb.g, rgb.b, a))
+      if header.pixelFormat == 1 then
+        local t = dxt3:read(f)
+        local cc = dxt1rgb(t.c0, t.c1)
+        for row = 1, 4 do
+          for col = 1, 4 do
+            local idx = 17 - ((row - 1) * 4 + col)
+            local a = t.alphaTable[idx] * 16
+            local rgb = cc[t.colorTable[idx]]
+            table.insert(lines[row], string.char(rgb.r, rgb.g, rgb.b, a))
+          end
+        end
+      elseif header.pixelFormat == 7 then
+        local t = dxt5:read(f)
+        local aa = dxt5alpha(t.a0, t.a1)
+        local cc = dxt1rgb(t.c0, t.c1)
+        for row = 1, 4 do
+          for col = 1, 4 do
+            local idx = 17 - ((row - 1) * 4 + col)
+            local a = aa[t.alphaTable[idx]]
+            local rgb = cc[t.colorTable[idx]]
+            table.insert(lines[row], string.char(rgb.r, rgb.g, rgb.b, a))
+          end
         end
       end
     end
