a4852640ebbb667a1c3cb334bbf0b2b2ab03874e
Ferronn <ferronn@ferronn.dev>
2022-03-31 08:37:43 -0500

    dxt3 blp support (#135)
    * rename dxt1alpha to dxt5alpha
    
    * refactor blp dxt1 color handling
    
    * dxt3

--- wowless/blp.lua
+++ wowless/blp.lua
@@ -14,6 +14,13 @@ local blpHeader = vstruct.compile([[
   palette: { 256*u4 }
 ]])
 
+local dxt3 = vstruct.compile([[
+  alphaTable: { [ 8 | 16*u4 ] }
+  c0: { [ 2 | r: u5 g: u6 b: u5 ] }
+  c1: { [ 2 | r: u5 g: u6 b: u5 ] }
+  colorTable: { [ 4 | 16*u2 ] }
+]])
+
 local dxt5 = vstruct.compile([[
   a0: u1
   a1: u1
@@ -35,16 +42,27 @@ local function dxt1color(c0, c1)
   return c2, c3
 end
 
+local function dxt1scale(c)
+  return {
+    r = c.r * 256 / 32,
+    g = c.g * 256 / 64,
+    b = c.b * 256 / 32,
+  }
+end
+
 local function dxt1rgb(c0, c1)
   local r2, r3 = dxt1color(c0.r, c1.r)
   local g2, g3 = dxt1color(c0.g, c1.g)
   local b2, b3 = dxt1color(c0.b, c1.b)
-  local c2 = { r = r2, g = g2, b = b2 }
-  local c3 = { r = r3, g = g3, b = b3 }
-  return c2, c3
+  return {
+    [0] = dxt1scale(c0),
+    [1] = dxt1scale(c1),
+    [2] = dxt1scale({ r = r2, g = g2, b = b2 }),
+    [3] = dxt1scale({ r = r3, g = g3, b = b3 }),
+  }
 end
 
-local function dxt1alpha(a0, a1)
+local function dxt5alpha(a0, a1)
   if a0 > a1 then
     return {
       [0] = a0,
@@ -77,7 +95,7 @@ local function read(filename)
   assert(header.version == 1)
   assert(header.colorEncoding == 2) -- DXT
   assert(header.alphaSize == 8)
-  assert(header.pixelFormat == 7) -- DXT5
+  assert(header.pixelFormat == 1 or header.pixelFormat == 7) -- DXT5
   assert(header.hasMips == 1 or header.hasMips == 17)
   assert(header.width % 4 == 0)
   assert(header.height % 4 == 0)
@@ -87,16 +105,28 @@ local function read(filename)
   for _ = 1, header.height / 4 do
     local lines = { {}, {}, {}, {} }
     for _ = 1, header.width / 4 do
-      local t = dxt5:read(f)
-      local c2, c3 = dxt1rgb(t.c0, t.c1)
-      local aa = dxt1alpha(t.a0, t.a1)
-      for row = 1, 4 do
-        for col = 1, 4 do
-          local idx = 17 - ((row - 1) * 4 + col)
-          local a = aa[t.alphaTable[idx]]
-          local cx = t.colorTable[idx]
-          local rgb = cx == 0 and t.c0 or cx == 1 and t.c1 or cx == 2 and c2 or c3
-          table.insert(lines[row], string.char(rgb.r * 256 / 32, rgb.g * 256 / 64, rgb.b * 256 / 32, a))
+      if header.pixelFormat == 1 then
+        local t = dxt3:read(f)
+        local cc = dxt1rgb(t.c0, t.c1)
+        for row = 1, 4 do
+          for col = 1, 4 do
+            local idx = 17 - ((row - 1) * 4 + col)
+            local a = t.alphaTable[idx] * 16
+            local rgb = cc[t.colorTable[idx]]
+            table.insert(lines[row], string.char(rgb.r, rgb.g, rgb.b, a))
+          end
+        end
+      elseif header.pixelFormat == 7 then
+        local t = dxt5:read(f)
+        local aa = dxt5alpha(t.a0, t.a1)
+        local cc = dxt1rgb(t.c0, t.c1)
+        for row = 1, 4 do
+          for col = 1, 4 do
+            local idx = 17 - ((row - 1) * 4 + col)
+            local a = aa[t.alphaTable[idx]]
+            local rgb = cc[t.colorTable[idx]]
+            table.insert(lines[row], string.char(rgb.r, rgb.g, rgb.b, a))
+          end
         end
       end
     end
