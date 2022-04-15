6ae38afbb5a1f832d6d54af09a004e307eaf8976
Ferronn <ferronn@ferronn.dev>
2022-04-15 17:02:03 +0000

    implement Texture:SetAtlas

---
+++ data/uiobjects/Texture/SetAtlas.lua
@@ -0,0 +1,15 @@
+return (function(self, name)
+  if name == nil then
+    -- TODO error here instead of silently ignoring
+    return
+  end
+  local t = api.env.C_Texture.GetAtlasInfo(name)
+  if t == nil then
+    -- TODO determine if we should error here instead of silently ignoring
+    return
+  end
+  m(self, 'SetTexture', t.filename or tostring(t.file))
+  m(self, 'SetHorizTile', t.tilesHorizontally)
+  m(self, 'SetVertTile', t.tilesVertically)
+  m(self, 'SetTexCoord', t.leftTexCoord, t.rightTexCoord, t.topTexCoord, t.bottomTexCoord)
+end)(...)
