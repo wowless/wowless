87db095d2fc14ab592faf21fab1a919ba7cc0c95
Ferronn <ferronn@ferronn.dev>
2022-06-18 18:21:01 +0000

    tools/addons.lua: make unzip quieter

--- tools/addons.lua
+++ tools/addons.lua
@@ -12,7 +12,7 @@ mkdir -p extracts/addons
 > for repo, pattern in pairs(github) do
 gh release download -R $(repo) -D extracts/addonzips -p "$(pattern)"
 > end
-unzip -o -d extracts/addons 'extracts/addonzips/*'
+unzip -q -o -d extracts/addons 'extracts/addonzips/*'
 rm -rf extracts/addonzips
 ]],
   { _escape = '>', github = github, pairs = pairs }
