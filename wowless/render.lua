c77bba98adc541eddaeb57188035493beaa998ef
Ferronn <ferronn@ferronn.dev>
2022-07-04 19:58:21 +0000

    render textures with no texcoords

--- wowless/render.lua
+++ wowless/render.lua
@@ -5,10 +5,12 @@ local function render(data, screenWidth, screenHeight, authority, rootDir, outfi
     pwand:set_color(c)
     return pwand
   end
-  local red, green, blue = color('red'), color('green'), color('blue')
+  local red, blue = color('red'), color('blue')
   local dwand = magick.new_drawing_wand()
   dwand:set_fill_opacity(0)
   local conn = authority and require('wowless.http').connect(authority)
+  local mwand = magick.new_magick_wand()
+  assert(mwand:new_image(screenWidth, screenHeight, color('none')))
   for _, v in pairs(data) do
     if v.content.texture then
       local r = v.rect
@@ -21,9 +23,24 @@ local function render(data, screenWidth, screenHeight, authority, rootDir, outfi
           fpath = fpath .. '.blp'
         end
         local content = conn(fpath)
-        if content then
-          dwand:set_stroke_color(green)
-          dwand:rectangle(left, top, right, bottom)
+        local success, png = pcall(function()
+          return require('wowless.png').write(require('wowless.blp').read(content))
+        end)
+        local c = v.content.texture.coords
+        if
+          success
+          and c.blx == 0
+          and c.bly == 1
+          and c.brx == 1
+          and c.bry == 1
+          and c.tlx == 0
+          and c.tly == 0
+          and c.trx == 1
+          and c.try == 0
+        then
+          local twand = magick.new_magick_wand()
+          assert(twand:read_image_blob(png))
+          assert(mwand:composite_image(twand, magick.CompositeOperator.OverCompositeOp, left, top))
         else
           dwand:set_stroke_color(red)
           dwand:rectangle(left, top, right, bottom)
@@ -34,9 +51,7 @@ local function render(data, screenWidth, screenHeight, authority, rootDir, outfi
       end
     end
   end
-  local mwand = magick.new_magick_wand()
-  assert(mwand:new_image(screenWidth, screenHeight, color('none')))
-  mwand:draw_image(dwand)
+  assert(mwand:draw_image(dwand))
   assert(mwand:write_image(outfile))
 end
 
