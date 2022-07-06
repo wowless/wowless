faac83bce3c1fe4a6e2dcf46ada85ab41a13bee0
Ferronn <ferronn@ferronn.dev>
2022-07-06 18:40:14 +0000

    remove frame-to-rects functionality to render.lua

--- tools/render.lua
+++ tools/render.lua
@@ -1,2 +1,2 @@
 local t = require('wowapi.yaml').parseFile('frame0.yaml')
-require('wowless.render').render(t, 1280, 720, 'localhost:8080', 'extracts/wow_classic_era', 'frame0.png')
+require('wowless.render').rects2png(t, 1280, 720, 'localhost:8080', 'extracts/wow_classic_era', 'frame0.png')
