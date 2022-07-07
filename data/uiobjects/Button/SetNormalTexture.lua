a3ba43e589baa5b1771c3358e32f1a7bf459a72f
Ferronn <ferronn@ferronn.dev>
2022-07-07 16:12:06 +0000

    hack to SetAllPoints on button normal textures

--- data/uiobjects/Button/SetNormalTexture.lua
+++ data/uiobjects/Button/SetNormalTexture.lua
@@ -3,4 +3,8 @@ return (function(self, tex)
   local t = toTexture(self, tex)
   ud.normalTexture = t
   t:SetShown(ud.buttonState == 'NORMAL')
+  -- TODO validate with reference
+  if t:GetParent() == self and t:GetNumPoints() == 0 then
+    t:SetAllPoints()
+  end
 end)(...)
