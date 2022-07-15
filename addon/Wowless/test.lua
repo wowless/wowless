6fbc9d3a45bbd4409c9c3dd8dead6918ed0cb506
Ferronn <ferronn@ferronn.dev>
2022-07-15 20:49:40 +0000

    fix format tests, again

--- addon/Wowless/test.lua
+++ addon/Wowless/test.lua
@@ -237,12 +237,12 @@ local syncTests = function()
           end))
         end,
         ['format handles %f'] = function()
-          assertEquals('inf', format('%f', 1 / 0):sub(-3))
-          assertEquals('nan', format('%f', 0 / 0):sub(-3))
+          assertEquals('inf', format('%f', math.huge):sub(-3))
+          assertEquals('nan', format('%f', -math.sin(math.huge)):sub(-3))
         end,
         ['format handles %F'] = function()
-          assertEquals('inf', format('%F', 1 / 0):sub(-3))
-          assertEquals('nan', format('%F', 0 / 0):sub(-3))
+          assertEquals('inf', format('%F', math.huge):sub(-3))
+          assertEquals('nan', format('%F', -math.sin(math.huge)):sub(-3))
         end,
       }
     end,
