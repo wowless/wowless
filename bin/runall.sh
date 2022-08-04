70647f835f66b5c87e2558421c4ad575dd59bf15
Ferronn <ferronn@ferronn.dev>
2022-08-04 13:42:46 +0000

    remove parallelization from runall.sh

--- bin/runall.sh
+++ bin/runall.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 mkdir -p out
 f() {
-  for p in wow wowt wow_classic wow_classic_era wow_classic_era_ptr wow_classic_ptr; do
-    bin/run.sh -p $p &> out/$p.txt &
+  for p in wow wowt wow_classic wow_classic_era wow_classic_era_ptr wow_classic_ptr wow_beta wow_classic_beta; do
+    echo -n "$p: "
+    if bin/run.sh -p "$p" -e1 &> out/"$p".txt; then
+      echo success
+    else
+      echo failure
+    fi
   done
-  wait
 }
 time f
 ls -l out
