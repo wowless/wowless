2c8fbf983d76ba51886992730c9aec7c7d12a06e
Ferronn <ferronn@ferronn.dev>
2022-08-05 19:08:56 +0000

    replace build/test with mkninja and ninja

--- bin/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-(cd tainted-lua && cmake --preset linux && cmake --build --preset linux)
-luarocks build --no-install
--- bin/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-bin/build.sh
-tainted-lua/build/linux/bin/Release/lua5.1 tools/gentest.lua
-busted -- "$@"
