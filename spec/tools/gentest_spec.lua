cf08c32eb60060a194ff90ebe9776df484170267
Ferronn <ferronn@ferronn.dev>
2022-08-16 16:26:33 +0000

    gentest: remove spec now that we do not check in generated files

--- spec/tools/gentest_spec.lua
+++
@@ -1,9 +0,0 @@
-describe('gentest', function()
-  local filemap = loadfile('tools/gentest.lua')('-n')
-  for k, v in pairs(filemap) do
-    it(k .. ' is consistent', function()
-      local addon = require('pl.file').read(k)
-      assert(v == addon)
-    end)
-  end
-end)
