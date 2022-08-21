7cfe7394a0eaccade951f25a98696d5b0c6b7167
Ferronn <ferronn@ferronn.dev>
2022-08-21 20:48:48 +0000

    CreateTexture: handle additional arguments

--- data/uiobjects/Frame/CreateTexture.lua
+++ data/uiobjects/Frame/CreateTexture.lua
@@ -1,3 +1,14 @@
-return (function(self, name)
-  return api.CreateUIObject('texture', name, self)
+return (function(self, name, layer, inherits, sublayer)
+  -- TODO unify with api
+  local tmpls = {}
+  for templateName in string.gmatch(inherits or '', '[^, ]+') do
+    local template = api.templates[string.lower(templateName)]
+    assert(template, 'unknown template ' .. templateName)
+    table.insert(tmpls, template)
+  end
+  local tex = api.CreateUIObject('texture', name, self, nil, tmpls)
+  if layer then
+    tex:SetDrawLayer(layer, sublayer)
+  end
+  return tex
 end)(...)
