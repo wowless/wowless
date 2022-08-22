44d6d4045a850b50b641afc67f77244fe090e6a1
Ferronn <ferronn@ferronn.dev>
2022-08-22 00:59:33 +0000

    hack fixes for GetItemInteractionInfo

--- data/impl/C_ItemInteraction.GetItemInteractionInfo.lua
+++ data/impl/C_ItemInteraction.GetItemInteractionInfo.lua
@@ -1,6 +1,6 @@
 -- TODO: nil when UI is not open; use appropariate row based on open UI
 local uiiteminteractionByID = ...
-local row = uiiteminteractionByID(1)
+local row = uiiteminteractionByID(3)
 return {
   buttonText = row.ButtonText_lang,
   buttonTooltip = row.Field_9_1_5_40196_004_lang,
@@ -10,8 +10,8 @@ return {
   currencyTypeId = row.CurrencyTypeID,
   description = row.Description_lang,
   dropInSlotSoundKitId = row.DropInSlotSoundKitID,
-  flags = row.Field_8_3_0_32414_012,
-  interactionType = row.ItemInteractionFrameType,
+  flags = row.ItemInteractionFrameType, -- TODO fix dbdef
+  interactionType = row.Field_8_3_0_32414_012, -- TODO fix dbdef
   openSoundKitID = row.OpenSoundKitID,
   textureKit = row.UiTextureKitID,
   titleText = row.TitleText_lang,
