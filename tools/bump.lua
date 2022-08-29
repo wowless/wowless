dc7f9df10f3ecb866ca9726b03ec53c2eedfbbc3
Ferronn <ferronn@ferronn.dev>
2022-08-29 15:57:44 +0000

    bump: only write if different

--- tools/bump.lua
+++ tools/bump.lua
@@ -2,11 +2,15 @@ local casc = require('casc')
 local yaml = require('wowapi.yaml')
 local builds = require('wowapi.data').builds
 for p, b in pairs(builds) do
+  local old = yaml.pprint(b)
   local bkey, _, _, version = casc.cdnbuild('http://us.patch.battle.net:1119/' .. p, 'us')
   local v1, v2, v3, v4 = version:match('^(%d+)%.(%d+)%.(%d+)%.(%d+)$')
   b.build = v4
   b.hash = bkey
   b.version = ('%s.%s.%s'):format(v1, v2, v3)
   b.tocversion = tonumber(v1) * 10000 + tonumber(v2) * 100 + tonumber(v3)
-  require('pl.file').write('data/products/' .. p .. '/build.yaml', yaml.pprint(b))
+  local new = yaml.pprint(b)
+  if old ~= new then
+    require('pl.file').write('data/products/' .. p .. '/build.yaml', new)
+  end
 end
