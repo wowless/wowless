ee637d3a6b57237d669f316ef8dbbb5f599d428c
Ferronn <ferronn@ferronn.dev>
2022-09-15 01:19:19 +0000

    marshal tables

--- spec/wowless/sandbox_spec.lua
+++ spec/wowless/sandbox_spec.lua
@@ -1,20 +1,35 @@
 describe('sandbox', function()
   local lib = require('wowless.sandbox')
-  it('does not crash', function()
+  describe('marshalling', function()
+    local works = {
+      array = '{ 1, "2", 3 }',
+      emptytable = '{}',
+      multiple = '1, "2", 3',
+      nested = '{ {} }',
+      nested2 = '{ { x = { y = { z = 42 } } } }',
+      ['nil'] = 'nil',
+      onenumber = '42',
+      record = '{ a = "x", b = 42 }',
+      ['true'] = 'true',
+    }
+    local fails = {
+      ['function'] = 'function() end',
+      userdata = 'newproxy()',
+    }
     local sandbox = lib.create()
-    assert.same({ 42 }, { sandbox:eval('return 42') })
-    assert.same({ 1, '2', 3 }, { sandbox:eval('return 1, "2", 3') })
-    assert.same({ nil }, { sandbox:eval('return nil') })
-    assert.same({ true }, { sandbox:eval('return true') })
-    assert.errors(function()
-      sandbox:eval('return {}')
-    end)
-    assert.errors(function()
-      sandbox:eval('return function() end')
-    end)
-    assert.errors(function()
-      sandbox:eval('return newproxy()')
-    end)
+    for k, v in pairs(works) do
+      it(k .. ' works', function()
+        local f = 'return ' .. v
+        assert.same({ loadstring(f)() }, { sandbox:eval(f) })
+      end)
+    end
+    for k, v in pairs(fails) do
+      it(k .. ' fails', function()
+        assert.errors(function()
+          sandbox:eval('return ' .. v)
+        end)
+      end)
+    end
   end)
   it('does not support require', function()
     assert.same(true, lib.create():eval('return require == nil'))
--- wowless/sandbox.c
+++ wowless/sandbox.c
@@ -24,6 +24,41 @@ static int wowless_sandbox_gc(lua_State *L) {
   return 0;
 }
 
+static void copy_value(lua_State *L, lua_State *S, int i) {
+  int ty = lua_type(S, i);
+  switch (ty) {
+    case LUA_TNIL:
+      lua_pushnil(L);
+      break;
+    case LUA_TBOOLEAN:
+      lua_pushboolean(L, lua_toboolean(S, i));
+      break;
+    case LUA_TNUMBER:
+      lua_pushnumber(L, lua_tonumber(S, i));
+      break;
+    case LUA_TSTRING:
+      lua_pushstring(L, lua_tostring(S, i));
+      break;
+    case LUA_TTABLE:
+      lua_newtable(L);
+      lua_pushvalue(S, i);
+      lua_pushnil(S);
+      while (lua_next(S, -2) != 0) {
+        copy_value(L, S, -2);
+        copy_value(L, S, -1);
+        lua_rawset(L, -3);
+        lua_pop(S, 1);
+      }
+      lua_pop(S, 1);
+      break;
+    default:
+      lua_pushstring(L, "invalid type: ");
+      lua_pushstring(L, lua_typename(S, ty));
+      lua_concat(L, 2);
+      lua_error(L);
+  }
+}
+
 static int wowless_sandbox_eval(lua_State *L) {
   lua_State *S = check_sandbox(L);
   const char *str = luaL_checkstring(L, 2);
@@ -34,26 +69,7 @@ static int wowless_sandbox_eval(lua_State *L) {
   }
   int n = lua_gettop(S);
   for (int i = 1; i <= n; ++i) {
-    int ty = lua_type(S, i);
-    switch (ty) {
-      case LUA_TNUMBER:
-        lua_pushnumber(L, lua_tonumber(S, i));
-        break;
-      case LUA_TSTRING:
-        lua_pushstring(L, lua_tostring(S, i));
-        break;
-      case LUA_TBOOLEAN:
-        lua_pushboolean(L, lua_toboolean(S, i));
-        break;
-      case LUA_TNIL:
-        lua_pushnil(L);
-        break;
-      default:
-        lua_pushstring(L, "invalid type: ");
-        lua_pushstring(L, lua_typename(S, ty));
-        lua_concat(L, 2);
-        lua_error(L);
-    }
+    copy_value(L, S, i);
   }
   return n;
 }
