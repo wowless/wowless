05141da8dd9654806b7db52d8405ce32f1756dfc
Ferronn <ferronn@ferronn.dev>
2022-09-15 14:51:50 +0000

    apply

--- wowless/sandbox.c
+++ wowless/sandbox.c
@@ -5,25 +5,6 @@ extern void luaL_openlibsx(lua_State *, int);
 
 static const char sandbox_metatable_name[] = "wowless sandbox";
 
-static int wowless_sandbox_create(lua_State *L) {
-  lua_State **p = lua_newuserdata(L, sizeof(*p));
-  luaL_getmetatable(L, sandbox_metatable_name);
-  lua_setmetatable(L, -2);
-  *p = luaL_newstate();
-  luaL_openlibsx(*p, 1); /* elune libs only */
-  return 1;
-}
-
-static lua_State *check_sandbox(lua_State *L) {
-  return *(lua_State **)luaL_checkudata(L, 1, sandbox_metatable_name);
-}
-
-static int wowless_sandbox_gc(lua_State *L) {
-  lua_State *S = check_sandbox(L);
-  lua_close(S);
-  return 0;
-}
-
 static void copy_value(lua_State *L, lua_State *S, int i) {
   int ty = lua_type(S, i);
   switch (ty) {
@@ -57,6 +38,48 @@ static void copy_value(lua_State *L, lua_State *S, int i) {
       lua_concat(L, 2);
       lua_error(L);
   }
+}
+
+static int sandboxapply(lua_State *S) {
+  const char *str = luaL_checkstring(S, 1);
+  lua_State *L = (lua_State *)lua_touserdata(S, lua_upvalueindex(1));
+  int old_host_top = lua_gettop(L);
+  lua_getglobal(L, str);
+  int sandbox_top = lua_gettop(S);
+  for (int i = 2; i <= sandbox_top; ++i) {
+    copy_value(L, S, i);
+  }
+  if (lua_pcall(L, sandbox_top - 1, LUA_MULTRET, 0) != 0) {
+    copy_value(S, L, -1);
+    lua_error(S);
+  }
+  int host_top = lua_gettop(L);
+  for (int i = old_host_top + 1; i <= host_top; ++i) {
+    copy_value(S, L, i);
+  }
+  return host_top - old_host_top;
+}
+
+static int wowless_sandbox_create(lua_State *L) {
+  lua_State **p = lua_newuserdata(L, sizeof(*p));
+  luaL_getmetatable(L, sandbox_metatable_name);
+  lua_setmetatable(L, -2);
+  *p = luaL_newstate();
+  luaL_openlibsx(*p, 1); /* elune libs only */
+  lua_pushlightuserdata(*p, L);
+  lua_pushcclosure(*p, sandboxapply, 1);
+  lua_setglobal(*p, "apply");
+  return 1;
+}
+
+static lua_State *check_sandbox(lua_State *L) {
+  return *(lua_State **)luaL_checkudata(L, 1, sandbox_metatable_name);
+}
+
+static int wowless_sandbox_gc(lua_State *L) {
+  lua_State *S = check_sandbox(L);
+  lua_close(S);
+  return 0;
 }
 
 static int wowless_sandbox_eval(lua_State *L) {
