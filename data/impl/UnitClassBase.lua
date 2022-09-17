d6cdc3c80f42159209b7af002467a31f3bbc26ae
Ferronn <ferronn@ferronn.dev>
2022-09-17 03:12:36 +0000

    convert UnitClass/UnitClassBase to sql

--- data/impl/UnitClassBase.lua
+++ data/impl/UnitClassBase.lua
@@ -1,5 +1,4 @@
-local chrclasses, unit = ...
-local row = unit and chrclasses(unit.class)
-if row then
-  return row.Filename, row.ID
+local sql, unit = ...
+if unit then
+  return sql(unit.class)
 end
