b6cce11dc76f07807e341120b3fbd55a092b50e5
Ferronn <ferronn@ferronn.dev>
2022-09-17 22:40:11 +0000

    convert C_ItemInteraction.GetItemInteractionInfo to sql

--- data/impl/C_ItemInteraction.GetItemInteractionInfo.lua
+++ data/impl/C_ItemInteraction.GetItemInteractionInfo.lua
@@ -1,19 +1,3 @@
--- TODO: nil when UI is not open; use appropariate row based on open UI
-local uiiteminteractionByID = ...
-local row = uiiteminteractionByID(3)
-return {
-  buttonText = row.ButtonText_lang,
-  buttonTooltip = row.Field_9_1_5_40196_004_lang,
-  closeSoundKitID = row.CloseSoundKitID,
-  confirmationDescription = row.ConfirmationQuestion_lang,
-  cost = row.Cost,
-  currencyTypeId = row.CurrencyTypeID,
-  description = row.Description_lang,
-  dropInSlotSoundKitId = row.DropInSlotSoundKitID,
-  flags = row.ItemInteractionFrameType, -- TODO fix dbdef
-  interactionType = row.Field_8_3_0_32414_012, -- TODO fix dbdef
-  openSoundKitID = row.OpenSoundKitID,
-  textureKit = row.UiTextureKitID,
-  titleText = row.TitleText_lang,
-  tutorialText = row.TutorialText_lang,
-}
+-- TODO: nil when UI is not open; use appropriate row based on open UI
+local sql = ...
+return sql(3)
