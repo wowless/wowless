218e47c010e01acae37faced08f497ee41ae3ed7
Ferronn <ferronn@ferronn.dev>
2022-09-21 01:39:40 +0000

    make datalua injectable directly to api impls

--- data/impl/C_Console.GetAllCommands.lua
+++ data/impl/C_Console.GetAllCommands.lua
@@ -1,6 +1,6 @@
-local api = ...
+local datalua = ...
 local t = {}
-for k in require('pl.tablex').sort(api.datalua.cvars) do
+for k in require('pl.tablex').sort(datalua.cvars) do
   table.insert(t, { command = k })
 end
 return t
