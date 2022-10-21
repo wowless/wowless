464e04cc07cdd010228c5cae449434bb425ef48a
Ferronn <ferronn@ferronn.dev>
2022-10-21 16:44:22 +0000

    tools/bump: accept product arg

--- tools/bump.lua
+++ tools/bump.lua
@@ -1,8 +1,16 @@
 local casc = require('casc')
 local yaml = require('wowapi.yaml')
-local builds = require('wowapi.data').builds
-for p, b in pairs(builds) do
-  local old = yaml.pprint(b)
+local allProducts = require('wowless.util').productList()
+local args = (function()
+  local parser = require('argparse')()
+  parser:argument('products', 'product tags'):args('*'):choices(allProducts)
+  return parser:parse()
+end)()
+local products = next(args.products) and args.products or allProducts
+for _, p in ipairs(products) do
+  local fn = 'data/products/' .. p .. '/build.yaml'
+  local old = require('pl.file').read(fn)
+  local b = yaml.parse(old)
   local bkey, _, _, version = casc.cdnbuild('http://us.patch.battle.net:1119/' .. p, 'us')
   local v1, v2, v3, v4 = version:match('^(%d+)%.(%d+)%.(%d+)%.(%d+)$')
   b.build = v4
@@ -11,6 +19,6 @@ for p, b in pairs(builds) do
   b.tocversion = tonumber(v1) * 10000 + tonumber(v2) * 100 + tonumber(v3)
   local new = yaml.pprint(b)
   if old ~= new then
-    require('pl.file').write('data/products/' .. p .. '/build.yaml', new)
+    require('pl.file').write(fn, new)
   end
 end
