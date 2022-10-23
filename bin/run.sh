fda75d420b31533db13386fe7627b57bbb31fc8d
Ferronn <ferronn@ferronn.dev>
2022-10-23 17:06:54 +0000

    move elune to vendor/elune

--- bin/run.sh
+++ bin/run.sh
@@ -3,4 +3,4 @@ set -e
 product="$1"
 shift
 ninja "$product"
-elune/build/linux/bin/Release/lua5.1 wowless.lua -p "$product" "$@"
+vendor/elune/build/linux/bin/Release/lua5.1 wowless.lua -p "$product" "$@"
