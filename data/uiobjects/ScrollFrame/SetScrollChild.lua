9042ab781333d73a264fc0c87a3bad8a62fe31db
Ferronn <ferronn@ferronn.dev>
2022-12-23 19:29:49 +0000

    bump wow_classic_ptr

--- data/uiobjects/ScrollFrame/SetScrollChild.lua
+++ data/uiobjects/ScrollFrame/SetScrollChild.lua
@@ -1,7 +1,10 @@
 return (function(self, ...)
   assert(select('#', ...) == 1, 'SetScrollChild: wrong number of arguments')
   local scrollChild = ...
-  assert(type(scrollChild) == 'table' or build.flavor ~= 'Mainline', 'Usage: self:SetScrollChild(scrollChild)')
+  assert(
+    type(scrollChild) == 'table' or build.flavor ~= 'Mainline' and runtimeProduct ~= 'wow_classic_ptr',
+    'Usage: self:SetScrollChild(scrollChild)'
+  )
   if type(scrollChild) == 'string' then
     scrollChild = api.env[scrollChild]
   end
