ba5a9e1d7ff2af647d8409d2efc6b229ae85b9f5
Ferronn <ferronn@ferronn.dev>
2023-01-18 20:54:33 +0000

    better structure test, collect innerTypes

--- spec/data/structures_spec.lua
+++ spec/data/structures_spec.lua
@@ -1,57 +1,63 @@
 describe('structures', function()
   local parseYaml = require('wowapi.yaml').parseFile
+  local nonStructs = {
+    boolean = true,
+    ['function'] = true,
+    ['nil'] = true,
+    number = true,
+    ['oneornil'] = true,
+    string = true,
+    table = true,
+    unit = true,
+    unknown = true,
+    userdata = true,
+  }
   for _, p in ipairs(require('wowless.util').productList()) do
     describe(p, function()
-      local refs = {}
-      local function ref(xs)
-        for _, x in ipairs(xs) do
-          refs[x.type] = true
-          if x.innerType then
-            refs[x.innerType] = true
-          end
+      local function ref(refs, x)
+        if x and not nonStructs[x] then
+          refs[x] = true
         end
       end
+      local function reflist(refs, xs)
+        for _, x in ipairs(xs or {}) do
+          ref(refs, x.type)
+          ref(refs, x.innerType)
+        end
+      end
+      local expected = {}
       for _, api in pairs(parseYaml('data/products/' .. p .. '/apis.yaml')) do
         for _, il in ipairs(api.inputs or {}) do
-          ref(il)
+          reflist(expected, il)
         end
-        ref(api.outputs or {})
+        reflist(expected, api.outputs)
       end
       for _, v in pairs(parseYaml('data/products/' .. p .. '/events.yaml')) do
-        ref(v.payload or {})
+        reflist(expected, v.payload)
       end
       local structures = parseYaml('data/products/' .. p .. '/structures.yaml')
-      local closure = {}
-      local function close(t)
-        if structures[t] and not closure[t] then
-          closure[t] = true
-          for _, f in pairs(structures[t]) do
-            close(f.type)
-            if f.innerType then
-              close(f.innerType)
-            end
-          end
+      local actual = {}
+      for k, v in pairs(structures) do
+        actual[k] = true
+        for _, fv in pairs(v) do
+          ref(expected, fv.type)
+          ref(expected, fv.innerType)
         end
       end
-      for k in pairs(refs) do
-        close(k)
-      end
-      --[[
-      -- Uncomment this to fix up the structures file.
-      local x = {}
-      for k in pairs(closure) do
-        x[k] = structures[k]
-      end
-      require('pl.file').write('data/products/' .. p .. '/structures.yaml', require('wowapi.yaml').pprint(x))
-]]
-      --
-      for k in pairs(structures) do
-        describe(k, function()
-          it('is in use', function()
-            assert.True(closure[k])
+      describe('exists', function()
+        for k in pairs(expected) do
+          it(k, function()
+            assert.True(actual[k])
           end)
-        end)
-      end
+        end
+      end)
+      describe('reffed', function()
+        for k in pairs(actual) do
+          it(k, function()
+            assert.True(expected[k])
+          end)
+        end
+      end)
     end)
   end
 end)
