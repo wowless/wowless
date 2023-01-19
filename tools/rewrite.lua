07064ea543ba78178a7e301b3c99a8b64b5f65cc
Ferronn <ferronn@ferronn.dev>
2023-01-19 04:14:23 +0000

    GetTickTime prototype

--- tools/rewrite.lua
+++ tools/rewrite.lua
@@ -1,47 +1,15 @@
-local lfs = require('lfs')
-local pf = require('pl.file')
+local writeFile = require('pl.file').write
 local yaml = require('wowapi.yaml')
 
 local function apiRewriter(fn)
-  for filename in lfs.dir('data/api') do
-    if filename:sub(-5) == '.yaml' then
-      local api = yaml.parseFile('data/api/' .. filename)
-      local newapi = fn(api)
-      if newapi then
-        pf.write('data/api/' .. filename, yaml.pprint(newapi))
-      end
-    end
-  end
-end
-
-local function uiobjectRewriter(fn)
-  for d in lfs.dir('data/uiobjects') do
-    if d ~= '.' and d ~= '..' then
-      local filename = ('data/uiobjects/%s/%s.yaml'):format(d, d)
-      local cfg = yaml.parseFile(filename)
-      local newcfg = fn(cfg)
-      if newcfg then
-        pf.write(filename, yaml.pprint(newcfg))
-      end
-    end
-  end
-end
-
-local function xmlRewriter(fn)
-  for filename in lfs.dir('data/xml') do
-    if filename:sub(-5) == '.yaml' then
-      local xml = yaml.parseFile('data/xml/' .. filename)
-      local newxml = fn(xml)
-      if newxml then
-        pf.delete('data/xml/' .. filename)
-        pf.write('data/xml/' .. newxml.name .. '.yaml', yaml.pprint(newxml))
-      end
-    end
+  for _, p in ipairs(require('wowless.util').productList()) do
+    local filename = 'data/products/' .. p .. '/apis.yaml'
+    local data = yaml.parseFile(filename)
+    fn(p, data)
+    writeFile(filename, yaml.pprint(data))
   end
 end
 
 return {
   api = apiRewriter,
-  uiobject = uiobjectRewriter,
-  xml = xmlRewriter,
 }
