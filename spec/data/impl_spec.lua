f9ae38b418b0f7a10625156af10189822c9a0cda
Ferronn <ferronn@ferronn.dev>
2023-01-19 19:43:28 +0000

    better impl spec

--- spec/data/impl_spec.lua
+++ spec/data/impl_spec.lua
@@ -1,10 +1,17 @@
 describe('impl', function()
-  for filename in require('lfs').dir('data/impl') do
-    if filename ~= '.' and filename ~= '..' then
-      describe(filename, function()
-        assert(filename:sub(-4) == '.lua', 'invalid file ' .. filename)
-        assert(loadfile('data/impl/' .. filename))
-      end)
+  it('has the right files', function()
+    local expected = {}
+    for _, p in ipairs(require('wowless.util').productList()) do
+      for _, v in pairs(require('wowapi.yaml').parseFile('data/products/' .. p .. '/apis.yaml')) do
+        if v.impl then
+          expected['data/impl/' .. v.impl .. '.lua'] = true
+        end
+      end
     end
-  end
+    local actual = {}
+    for _, f in ipairs(require('pl.dir').getfiles('data/impl')) do
+      actual[f] = true
+    end
+    assert.same(expected, actual)
+  end)
 end)
