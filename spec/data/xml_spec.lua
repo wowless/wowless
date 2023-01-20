7465449362e36f3ae1f36bc051dba0374064ba87
Ferronn <ferronn@ferronn.dev>
2023-01-20 20:50:16 +0000

    remove very stale xml_spec

--- spec/data/xml_spec.lua
+++
@@ -1,8 +0,0 @@
-describe('xml', function()
-  local lang = require('wowapi.data').xml
-  it('has no entries with a field named "type"', function()
-    for k, v in pairs(lang) do
-      assert.True(not v.fields or not v.fields.type, k)
-    end
-  end)
-end)
