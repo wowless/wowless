2b1adbf2d7bc4039193a81b9f090458e7877c8fd
Ferronn <ferronn@ferronn.dev>
2023-01-27 17:04:11 +0000

    prep: unify stub creation

--- tools/prep.lua
+++ tools/prep.lua
@@ -13,84 +13,81 @@ local function readFile(...)
 end
 local plprettywrite = require('pl.pretty').write
 
-local defaultOutputs = {
-  boolean = 'false',
-  ['function'] = 'function() end',
-  ['nil'] = 'nil',
-  number = '1',
-  oneornil = 'nil',
-  string = '\'\'',
-  table = '{}',
-  unknown = 'nil',
-}
-
-local structureDefaults = {}
-
-local typeDefault = (function()
+local specDefault = (function()
+  local defaultOutputs = {
+    boolean = 'false',
+    ['function'] = 'function() end',
+    ['nil'] = 'nil',
+    number = '1',
+    oneornil = 'nil',
+    string = '\'\'',
+    table = '{}',
+    unknown = 'nil',
+  }
+  local structureDefaults = {}
   local structures = parseYaml('data/products/' .. product .. '/structures.yaml')
-  local function typeDefault(ty)
+  local specDefault
+  local function valstruct(name, mixin)
+    if not structureDefaults[name] then
+      local st = assert(structures[name], name)
+      local t = {}
+      for fname, field in require('pl.tablex').sort(st) do
+        local v = specDefault(field)
+        if v ~= 'nil' then
+          table.insert(t, ('[%q]=%s'):format(fname, v))
+        end
+      end
+      structureDefaults[name] = '{' .. table.concat(t, ',') .. '}'
+    end
+    local v = structureDefaults[name]
+    return mixin and ('Mixin(%s,%s)'):format(v, mixin) or v
+  end
+  local function valstr(value)
+    local ty = type(value)
+    if ty == 'number' or ty == 'boolean' then
+      return tostring(value)
+    elseif ty == 'string' then
+      return string.format('%q', value)
+    elseif ty == 'table' then
+      return plprettywrite(value)
+    else
+      error('unsupported stub value type ' .. ty)
+    end
+  end
+  function specDefault(spec)
+    if spec.stub ~= nil then
+      return valstr(spec.stub)
+    end
+    if spec.default ~= nil then
+      return valstr(spec.default)
+    end
+    if spec.nilable then
+      return 'nil'
+    end
+    local ty = assert(spec.type, 'spec missing a type')
     if defaultOutputs[ty] then
       return defaultOutputs[ty]
     end
     if ty.arrayof then
-      return '{' .. typeDefault(ty.arrayof) .. '}'
+      return '{' .. specDefault({ type = ty.arrayof }) .. '}'
     end
     if ty.structure then
-      if not structureDefaults[ty.structure] then
-        local st = assert(structures[ty.structure], ty.structure)
-        local t = {}
-        for fname, field in require('pl.tablex').sort(st) do
-          local v
-          if field.stub then
-            assert(field.type == 'string', 'only string stubs supported in structures')
-            assert(type(field.stub) == 'string', 'only string stubs supported in structures')
-            v = string.format('%q', field.stub)
-          elseif field.nilable then
-            v = 'nil'
-          else
-            v = typeDefault(field.type)
-          end
-          if v ~= 'nil' then
-            table.insert(t, ('[%q]=%s'):format(fname, v))
-          end
-        end
-        structureDefaults[ty.structure] = '{' .. table.concat(t, ',') .. '}'
-      end
-      local v = structureDefaults[ty.structure]
-      return ty.mixin and ('Mixin(%s,%s)'):format(v, ty.mixin) or v
+      return valstruct(ty.structure, ty.mixin)
+    end
+    -- TODO remove this when api specs are refactored
+    if structures[ty] then
+      return valstruct(ty, spec.mixin)
     end
     error('unexpected type: ' .. require('pl.pretty').write(ty))
   end
-  for name in pairs(structures) do
-    typeDefault({ structure = name })
-  end
-  return typeDefault
+  return specDefault
 end)()
 
 local getStub = (function()
   return function(sig)
     local rets = {}
     for _, out in ipairs(sig) do
-      local v
-      if out.stub or out.default then
-        local value = out.stub or out.default
-        local ty = type(value)
-        if ty == 'number' or ty == 'boolean' then
-          v = tostring(value)
-        elseif ty == 'string' then
-          v = string.format('%q', value)
-        elseif ty == 'table' then
-          v = plprettywrite(value)
-        else
-          error('unsupported stub value type ' .. ty)
-        end
-      elseif out.nilable then
-        v = 'nil'
-      else
-        v = defaultOutputs[out.type] or structureDefaults[out.type]
-      end
-      assert(v, ('invalid output type %q'):format(out.type))
-      table.insert(rets, v)
+      table.insert(rets, specDefault(out))
     end
     return 'return ' .. table.concat(rets, ', ')
   end
@@ -148,7 +145,7 @@ for k, v in pairs(parseYaml('data/products/' .. product .. '/events.yaml')) do
     payload = v.payload and (function()
       local t = {}
       for _, f in ipairs(v.payload or {}) do
-        table.insert(t, typeDefault(f.type))
+        table.insert(t, specDefault(f))
       end
       return 'return ' .. table.concat(t, ',')
     end)(),
