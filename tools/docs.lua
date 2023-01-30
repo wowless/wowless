bfacdf8cae472702216e2ec9f16e72194e31db3d
Ferronn <ferronn@ferronn.dev>
2023-01-30 15:57:00 +0000

    docs: write everything always

--- tools/docs.lua
+++ tools/docs.lua
@@ -1,10 +1,6 @@
-local allTypes = { 'apis', 'events', 'structures' }
 local args = (function()
   local parser = require('argparse')()
   parser:argument('product', 'product to process')
-  parser:argument('type', 'type to write'):choices(allTypes)
-  parser:argument('filter', 'name filter'):default('')
-  parser:flag('--new', 'add missing objects')
   return parser:parse()
 end)()
 
@@ -13,10 +9,6 @@ local writeFile = require('pl.file').write
 local parseYaml = require('wowapi.yaml').parseFile
 local pprintYaml = require('wowapi.yaml').pprint
 local product = args.product
-
-local function enabled(t, k)
-  return k:sub(1, #args.filter) == args.filter and (args.new or t[k])
-end
 
 local function deref(t, ...)
   for i = 1, select('#', ...) do
@@ -172,128 +164,156 @@ local function split(name)
   end
 end
 
-local rewriters = {
-  apis = function()
-    local function insig(fn, ns)
-      local t = {}
-      for _, a in ipairs(fn.Arguments or {}) do
-        table.insert(t, {
-          default = a.Default,
-          name = a.Name,
-          nilable = a.Nilable or nil,
-          type = t2nty(a, ns),
-        })
-      end
-      return t
+local function rewriteApis()
+  local function insig(fn, ns)
+    local t = {}
+    for _, a in ipairs(fn.Arguments or {}) do
+      table.insert(t, {
+        default = a.Default,
+        name = a.Name,
+        nilable = a.Nilable or nil,
+        type = t2nty(a, ns),
+      })
     end
-    local function outsig(fn, ns)
-      local outputs = {}
-      for _, r in ipairs(fn.Returns or {}) do
-        table.insert(outputs, {
-          default = enum[r.Type] and enum[r.Type][r.Default] or r.Default,
-          name = r.Name,
-          nilable = r.Nilable or nil,
-          type = t2nty(r, ns),
-        })
-      end
-      return outputs
+    return t
+  end
+  local function outsig(fn, ns)
+    local outputs = {}
+    for _, r in ipairs(fn.Returns or {}) do
+      table.insert(outputs, {
+        default = enum[r.Type] and enum[r.Type][r.Default] or r.Default,
+        name = r.Name,
+        nilable = r.Nilable or nil,
+        type = t2nty(r, ns),
+      })
     end
-    local cfgskip = deref(config, 'apis', 'skip_namespaces') or {}
-    local function skip(apis, name)
-      local ns = split(name)
-      if ns and cfgskip[ns] then
-        return true
-      end
-      local api = apis[name]
-      if not api then
-        return false
-      end
-      if api.impl then
-        return true
-      end
-      for _, out in ipairs(api.outputs or {}) do
-        if out.stub then
-          return true
-        end
-      end
+    return outputs
+  end
+  local cfgskip = deref(config, 'apis', 'skip_namespaces') or {}
+  local function skip(apis, name)
+    local ns = split(name)
+    if ns and cfgskip[ns] then
+      return true
+    end
+    local api = apis[name]
+    if not api then
       return false
     end
-    local y = require('wowapi.yaml')
-    local f = 'data/products/' .. product .. '/apis.yaml'
-    local apis = y.parseFile(f)
-    local nss = {}
-    for name, fn in pairs(funcs) do
-      nss[split(name) or ''] = true
-      if enabled(apis, name) and not skip(apis, name) then
-        local ns = split(name)
-        apis[name] = {
-          inputs = { insig(fn, ns) },
-          outputs = outsig(fn, ns),
-        }
+    if api.impl then
+      return true
+    end
+    for _, out in ipairs(api.outputs or {}) do
+      if out.stub then
+        return true
       end
     end
-    for k in pairs(cfgskip) do
-      assert(nss[k], k .. ' in skip_namespaces but not in docs')
-    end
-    require('pl.file').write(f, y.pprint(apis))
-  end,
-
-  events = function()
-    local filename = ('data/products/%s/events.yaml'):format(product)
-    local neversent = deref(config, 'events', 'never_sent') or {}
-    local out = require('wowapi.yaml').parseFile(filename)
-    local seen = {}
-    for name, ev in pairs(events) do
-      seen[ev.LiteralName] = true
+    return false
+  end
+  local y = require('wowapi.yaml')
+  local f = 'data/products/' .. product .. '/apis.yaml'
+  local apis = y.parseFile(f)
+  local nss = {}
+  for name, fn in pairs(funcs) do
+    nss[split(name) or ''] = true
+    if not skip(apis, name) then
       local ns = split(name)
-      out[ev.LiteralName] = {
-        payload = (function()
-          if neversent[ev.LiteralName] then
-            return nil
-          end
-          local t = {}
-          for _, arg in ipairs(ev.Payload or {}) do
-            table.insert(t, {
-              name = arg.Name,
-              nilable = arg.Nilable or nil,
-              type = t2nty(arg, ns),
-            })
-          end
-          return t
-        end)(),
+      apis[name] = {
+        inputs = { insig(fn, ns) },
+        outputs = outsig(fn, ns),
       }
     end
-    for k in pairs(neversent) do
-      assert(seen[k], k .. ' is marked never_sent but not present in docs')
-    end
-    writeFile(filename, pprintYaml(out))
-  end,
+  end
+  for k in pairs(cfgskip) do
+    assert(nss[k], k .. ' in skip_namespaces but not in docs')
+  end
+  require('pl.file').write(f, y.pprint(apis))
+  return apis
+end
 
-  structures = function()
-    local stubs = {
-      FramePoint = 'CENTER',
+local function rewriteEvents()
+  local filename = ('data/products/%s/events.yaml'):format(product)
+  local neversent = deref(config, 'events', 'never_sent') or {}
+  local out = require('wowapi.yaml').parseFile(filename)
+  local seen = {}
+  for name, ev in pairs(events) do
+    seen[ev.LiteralName] = true
+    local ns = split(name)
+    out[ev.LiteralName] = {
+      payload = (function()
+        if neversent[ev.LiteralName] then
+          return nil
+        end
+        local t = {}
+        for _, arg in ipairs(ev.Payload or {}) do
+          table.insert(t, {
+            name = arg.Name,
+            nilable = arg.Nilable or nil,
+            type = t2nty(arg, ns),
+          })
+        end
+        return t
+      end)(),
     }
-    local filename = ('data/products/%s/structures.yaml'):format(product)
-    local out = require('wowapi.yaml').parseFile(filename)
-    for name, tab in pairs(tabs) do
-      if tab.Type == 'Structure' and enabled(out, name) then
-        local ns = split(name)
-        out[name] = (function()
-          local ret = {}
-          for _, f in ipairs(tab.Fields) do
-            ret[f.Name] = {
-              default = enum[f.Type] and enum[f.Type][f.Default] or f.Default,
-              nilable = f.Nilable or nil,
-              stub = deref(out, name, f.Name, 'stub') or stubs[f.Type],
-              type = t2nty(f, ns),
-            }
-          end
-          return ret
-        end)()
-      end
-    end
-    writeFile(filename, pprintYaml(out))
-  end,
-}
+  end
+  for k in pairs(neversent) do
+    assert(seen[k], k .. ' is marked never_sent but not present in docs')
+  end
+  writeFile(filename, pprintYaml(out))
+  return out
+end
 
-rewriters[args.type]()
+local function rewriteStructures(outApis, outEvents)
+  local stubs = {
+    FramePoint = 'CENTER',
+  }
+  local filename = ('data/products/%s/structures.yaml'):format(product)
+  local structures = require('wowapi.yaml').parseFile(filename)
+  for name, tab in pairs(tabs) do
+    if tab.Type == 'Structure' then
+      local ns = split(name)
+      structures[name] = (function()
+        local ret = {}
+        for _, f in ipairs(tab.Fields) do
+          ret[f.Name] = {
+            default = enum[f.Type] and enum[f.Type][f.Default] or f.Default,
+            nilable = f.Nilable or nil,
+            stub = deref(structures, name, f.Name, 'stub') or stubs[f.Type],
+            type = t2nty(f, ns),
+          }
+        end
+        return ret
+      end)()
+    end
+  end
+  local out = {}
+  -- Only write transitive closure of referenced structures.
+  local function processType(ty)
+    if ty.structure and not out[ty.structure] then
+      out[ty.structure] = structures[ty.structure]
+      for _, v in pairs(structures[ty.structure]) do
+        processType(v.type)
+      end
+    elseif ty.arrayof then
+      processType(ty.arrayof)
+    end
+  end
+  local function processList(xs)
+    for _, x in ipairs(xs or {}) do
+      processType(x.type)
+    end
+  end
+  for _, api in pairs(outApis) do
+    for _, ilist in ipairs(api.inputs or {}) do
+      processList(ilist)
+    end
+    processList(api.outputs)
+  end
+  for _, event in pairs(outEvents) do
+    processList(event.payload)
+  end
+  writeFile(filename, pprintYaml(out))
+end
+
+local outApis = rewriteApis()
+local outEvents = rewriteEvents()
+rewriteStructures(outApis, outEvents)
