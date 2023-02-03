72902767b57f30d5b63687a408e05f6b862de863
Ferronn <ferronn@ferronn.dev>
2023-02-03 20:06:56 +0000

    GetPoint arg is just tonumber or 1

--- data/uiobjects/Region/GetPoint.lua
+++ data/uiobjects/Region/GetPoint.lua
@@ -1,16 +1,5 @@
 return (function(self, index)
-  local idx
-  if type(index) == 'string' then
-    local i = 1
-    while not idx and i <= #u(self).points do
-      if u(self).points[i][1] == index then
-        idx = i
-      end
-      i = i + 1
-    end
-  else
-    idx = index or 1
-  end
+  local idx = tonumber(index) or 1
   if u(self).points[idx] then
     assert(type(u(self).points[idx][4]) ~= 'table')
     return unpack(u(self).points[idx])
