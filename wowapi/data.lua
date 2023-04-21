22d97eda4c8adee2548be33f3218ec05b84a3be6
Ferronn <ferronn@ferronn.dev>
2023-04-21 18:51:52 +0000

    use luafied yaml in wowapi.data

--- wowapi/data.lua
+++ wowapi/data.lua
@@ -1,6 +1,8 @@
 local extLoaders = {
   sql = require('pl.file').read,
-  yaml = require('wowapi.yaml').parseFile,
+  yaml = function(f)
+    return require('build/' .. f:sub(1, -6))
+  end,
 }
 
 local function loaddir(dir, ext)
