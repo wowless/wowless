baa0ee9a596116084e7c60576d9fa4de4a580635
Ferronn <ferronn@ferronn.dev>
2023-05-31 16:46:14 +0000

    use wrapped impl in EnumerateFrames test

--- spec/data/impl/EnumerateFrames_spec.lua
+++ spec/data/impl/EnumerateFrames_spec.lua
@@ -1,22 +1,24 @@
 describe('EnumerateFrames', function()
-  local fn = loadfile('data/impl/EnumerateFrames.lua')
-  local function collect(api)
+  local api = require('wowless.api').new(function() end, 0, 'wow')
+  local loader = require('wowless.loader').loader(api, { product = 'wow' })
+  require('wowless.env').init(api, loader)
+  local impl = api.impls.EnumerateFrames
+  local function collect()
     local t = {}
-    local f = fn(api)
+    local f = impl()
     while f ~= nil do
       table.insert(t, f)
-      f = fn(api, f)
+      f = impl(f)
     end
     return t
   end
   it('works', function()
-    local api = require('wowless.api').new(function() end, 0, 'wow')
-    assert.same({}, collect(api))
-    local f1 = api.CreateFrame('frame')
-    assert.same({ f1.luarep }, collect(api))
-    local f2 = api.CreateFrame('frame')
-    assert.same({ f1.luarep, f2.luarep }, collect(api))
-    local f3 = api.CreateFrame('frame')
-    assert.same({ f1.luarep, f2.luarep, f3.luarep }, collect(api))
+    assert.same({}, collect())
+    local f1 = api.impls.CreateFrame('frame')
+    assert.same({ f1 }, collect())
+    local f2 = api.impls.CreateFrame('frame')
+    assert.same({ f1, f2 }, collect())
+    local f3 = api.impls.CreateFrame('frame')
+    assert.same({ f1, f2, f3 }, collect())
   end)
 end)
