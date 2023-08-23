7b41bd6f64150df7c562a4cd74a1eeb4d024a263
Ferronn <ferronn@ferronn.dev>
2023-08-23 01:56:48 +0000

    bumpaddons: enable just bumping one

--- tools/bumpaddons.lua
+++ tools/bumpaddons.lua
@@ -1,13 +1,20 @@
-local addons = require('wowapi.yaml').parseFile('tools/addons.yaml')
-for _, v in pairs(addons) do
+local function fix(addon)
   local p = io.popen(([[
     gh api graphql -f query='query {
       repository(owner: "%s", name: "%s") {
         releases(first: 1) { nodes { tagName } }
       }
     }' --jq .data.repository.releases.nodes[].tagName
-  ]]):format(v.owner, v.repo))
-  v.tag = p:read('*a'):sub(1, -2)
+  ]]):format(addon.owner, addon.repo))
+  addon.tag = p:read('*a'):sub(1, -2)
   p:close()
+end
+local addons = require('wowapi.yaml').parseFile('tools/addons.yaml')
+if arg[1] then
+  fix(assert(addons[arg[1]]))
+else
+  for _, v in pairs(addons) do
+    fix(v)
+  end
 end
 require('pl.file').write('tools/addons.yaml', require('wowapi.yaml').pprint(addons))
