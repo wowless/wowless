69dbaa37d411084bf0752f13a91354758a2011f1
Ferronn <ferronn@ferronn.dev>
2023-09-07 01:11:53 +0000

    add_strict_lua_executable, apply to prep

--- tools/luamain.lua
+++ tools/luamain.lua
@@ -8,7 +8,8 @@ local outfile = arg[1]
 local main = arg[2]
 local text = readfile(main):gsub('\\', '\\\\'):gsub('\n', '\\n\\\n'):gsub('"', '\\"')
 local preloads = {}
-for i = 3, #arg do
+local strict = arg[3] == '-strict'
+for i = strict and 4 or 3, #arg do
   table.insert(preloads, arg[i])
 end
 table.sort(preloads)
@@ -36,14 +37,26 @@ for _, p in ipairs(preloads) do
 end
 io.write([[
   lua_getglobal(L, "package");
+]])
+io.write(strict and '' or [[
   lua_pushstring(L, "./?.lua");
   lua_setfield(L, -2, "path");
+]])
+io.write([[
   lua_getfield(L, -1, "loaders");
+]])
+io.write(strict and [[
+  lua_createtable(L, 1, 0);
+  lua_rawgeti(L, -2, 1);
+  lua_rawseti(L, -2, -1);
+]] or [[
   lua_createtable(L, 2, 0);
   lua_rawgeti(L, -2, 1);
   lua_rawseti(L, -2, -1);
   lua_rawgeti(L, -2, 2);
   lua_rawseti(L, -2, -1);
+]])
+io.write([[
   lua_setfield(L, -1, "loaders");
   lua_pop(L, 2);
   lua_newtable(L);
