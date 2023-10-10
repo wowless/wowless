912f00adcd357b6ce6d1be464518ea01298d7a4c
Ferronn <ferronn@ferronn.dev>
2023-10-10 20:57:38 +0000

    remove sandbox struct for now

--- wowless/sandbox.c
+++ wowless/sandbox.c
@@ -1,89 +1,83 @@
 #include <lauxlib.h>
 #include <lualib.h>
 
-typedef struct {
-  lua_State *L;
-} sandbox;
-
 static const char sandbox_metatable[] = "wowless sandbox";
 
 static int sandbox_create(lua_State *L) {
-  lua_State *SL = luaL_newstate();
-  if (!SL) {
+  lua_State **p = lua_newuserdata(L, sizeof(*p));
+  *p = luaL_newstate();
+  if (!*p) {
     return luaL_error(L, "out of memory");
   }
-  sandbox *S = lua_newuserdata(L, sizeof(*S));
-  S->L = SL;
   luaL_getmetatable(L, sandbox_metatable);
   lua_setmetatable(L, -2);
   return 1;
 }
 
-static sandbox *sandbox_check(lua_State *L, int index) {
-  return luaL_checkudata(L, index, sandbox_metatable);
+static lua_State *sandbox_check(lua_State *L, int index) {
+  lua_State **p = luaL_checkudata(L, index, sandbox_metatable);
+  return *p;
 }
 
 static int sandbox_gc(lua_State *L) {
-  sandbox *S = sandbox_check(L, 1);
-  lua_close(S->L);
+  lua_State *S = sandbox_check(L, 1);
+  lua_close(S);
   return 0;
 }
 
 static int sandbox_dump(lua_State *L) {
-  sandbox *S = sandbox_check(L, 1);
-  lua_State *SL = S->L;
-  lua_pushvalue(SL, LUA_GLOBALSINDEX);
-  lua_pushnil(SL);
-  while (lua_gettop(SL) > 0) {
-    if (lua_next(SL, -2) != 0) {
-      puts(lua_tostring(SL, -2));
-      if (lua_type(SL, -1) == LUA_TTABLE) {
-        lua_pushnil(SL);
+  lua_State *S = sandbox_check(L, 1);
+  lua_pushvalue(S, LUA_GLOBALSINDEX);
+  lua_pushnil(S);
+  while (lua_gettop(S) > 0) {
+    if (lua_next(S, -2) != 0) {
+      puts(lua_tostring(S, -2));
+      if (lua_type(S, -1) == LUA_TTABLE) {
+        lua_pushnil(S);
         continue;
       }
     }
-    lua_pop(SL, 1);
+    lua_pop(S, 1);
   }
   return 0;
 }
 
 static int sandbox_import(lua_State *L) {
-  sandbox *S = sandbox_check(L, 1);
-  lua_State *SL = S->L;
+  lua_State *S = sandbox_check(L, 1);
   luaL_checktype(L, 2, LUA_TTABLE);
   lua_settop(L, 2);
   lua_pushnil(L);
-  lua_pushvalue(SL, LUA_GLOBALSINDEX);
+  lua_pushvalue(S, LUA_GLOBALSINDEX);
   while (lua_gettop(L) > 1) {
     if (lua_next(L, -2) == 0) {
-      if (lua_gettop(SL) > 1) {
-        lua_settable(SL, -3);
+      if (lua_gettop(S) > 1) {
+        lua_settable(S, -3);
       }
     } else if (!lua_type(L, -2) == LUA_TSTRING) {
-      lua_settop(SL, 0);
+      lua_settop(S, 0);
       luaL_error(L, "invalid key");
     } else {
-      lua_pushstring(SL, lua_tostring(L, -2));
+      lua_pushstring(S, lua_tostring(L, -2));
       switch (lua_type(L, -1)) {
         case LUA_TNUMBER:
-          lua_pushnumber(SL, lua_tonumber(L, -1));
+          lua_pushnumber(S, lua_tonumber(L, -1));
           break;
         case LUA_TSTRING:
-          lua_pushstring(SL, lua_tostring(L, -1));
+          lua_pushstring(S, lua_tostring(L, -1));
           break;
         case LUA_TTABLE:
           lua_pushnil(L);
-          lua_newtable(SL);
+          lua_newtable(S);
           continue;
         default:
-          lua_settop(SL, 0);
+          lua_settop(S, 0);
           luaL_error(L, "invald value");
       }
-      lua_settable(SL, -3);
+      lua_settable(S, -3);
     }
     lua_pop(L, 1);
   }
-  lua_pop(SL, 1);
+  lua_pop(S, 1);
   return 0;
 }
 
