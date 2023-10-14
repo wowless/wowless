ab24e9035a71aa9a62da01355c789fad841386e7
Ferronn <ferronn@ferronn.dev>
2023-10-14 09:14:24 -0500

    checkpoint

--- tools/apis2c.lua
+++ tools/apis2c.lua
@@ -9,13 +9,25 @@ for k, v in require('pl.tablex').sort((require('build.data.products.' .. p .. '.
         print(('  luaL_argcheck(L, lua_isboolean(L, %d), %d, 0);'):format(i, i))
       elseif input.type == 'string' or input.type == 'unit' or input.type == 'uiAddon' then
         print(('  luaL_%sstring(L, %d);'):format(input.nilable and 'opt' or 'check', i))
-      elseif input.type ~= 'unknown' then
+      elseif input.type == 'table' then
+        if input.nilable then
+          print(('  luaL_argcheck(L, lua_istable(L, %d) || lua_isnoneornil(L, %d), %d, 0);'):format(i, i, i))
+        else
+          print(('  luaL_argcheck(L, lua_istable(L, %d), %d, 0);'):format(i, i))
+        end
+      elseif input.type == 'unknown' then
+        if not input.nilable then
+          print(('  luaL_argcheck(L, !lua_isnoneornil(L, %d), %d, 0);'):format(i, i))
+        end
+      else
         error('wat inputs ' .. k)
       end
     end
     local outputs = v.outputs or {}
     for _, output in ipairs(outputs) do
-      if output.type == 'number' then
+      if output.nilable and not output.stub then
+        print('  lua_pushnil(L);')
+      elseif output.type == 'number' then
         print(('  lua_pushnumber(L, %d);'):format(output.stub or 1))
       elseif output.type == 'boolean' then
         print(('  lua_pushboolean(L, %s);'):format(output.stub and 1 or 0))
@@ -27,7 +39,7 @@ for k, v in require('pl.tablex').sort((require('build.data.products.' .. p .. '.
         assert(output.stub == nil)
         print('  lua_pushnil(L);')
       elseif output.type == 'table' then
-        assert(output.stub == nil)
+        assert(output.stub == nil, 'stub on ' .. k)
         print('  lua_newtable(L);')
       else
         error('wat outputs ' .. k)
