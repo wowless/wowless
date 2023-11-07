4b0546f32057f5519fdb614275643ec75a60820f
Ferronn <ferronn@ferronn.dev>
2023-11-07 18:50:56 +0000

    first cut at cmap-driven fetch

--- tools/fetch.lua
+++ tools/fetch.lua
@@ -5,137 +5,36 @@ local args = (function()
   return parser:parse()
 end)()
 local product = args.product
-
--- Don't let casc use any system backdoors.
-os.execute = function(...) -- luacheck: ignore
-  error('attempt to call execute(' .. table.concat({ ... }) .. ')')
-end
-
 local log = args.verbose and print or function() end
-
-local build = dofile('build/data/products/' .. product .. '/build.lua')
-local fdids = require('runtime.listfile')
-
+local cmap = dofile('build/products/' .. product .. '/cmap.lua')
 local path = require('path')
 path.mkdir('cache')
-
-local handle = (function()
-  local casc = require('casc')
-  local _, cdn, ckey = casc.cdnbuild('http://us.patch.battle.net:1119/' .. product, 'us')
-  local handle, err = casc.open({
-    bkey = build.hash,
-    cache = 'cache',
-    cacheFiles = true,
-    cdn = cdn,
-    ckey = ckey,
-    keys = require('runtime.tactkeys'),
-    locale = casc.locale.US,
-    log = log,
-    zerofillEncryptedChunks = true,
-  })
-  if not handle then
-    print('unable to open ' .. build.hash .. ': ' .. err)
-    os.exit(1)
-  end
-  return handle
-end)()
-
-local function normalizePath(p)
-  -- path.normalize does not normalize x/../y to y.
-  -- Unfortunately, we need exactly that behavior for Interface_Vanilla etc.
-  -- links in per-product TOCs. We hack around it here by adding an extra dir.
-  return path.normalize('a/' .. p):sub(3)
-end
-
-local function joinRelative(relativeTo, suffix)
-  return normalizePath(path.join(path.dirname(relativeTo), suffix))
-end
-
-local outdir = path.join('extracts', product)
+local outdir = path.join('extracts2', product)
 if path.isdir(outdir) then
   require('pl.dir').rmtree(outdir)
 end
-
-local save = (function()
-  local saved = {}
-  return function(fn, content)
-    assert(content, 'attempting to write nil content for ' .. fn)
-    if not saved[fn:lower()] then
-      log('writing', fn)
-      path.mkdir(path.dirname(path.join(outdir, fn)))
-      require('pl.file').write(path.join(outdir, fn), content)
-      saved[fn:lower()] = true
-    end
+local fetch = (function()
+  local http = require('socket.http')
+  local ltn12 = require('ltn12')
+  return function(v)
+    local h = v.archive
+    local suffix = ('%s/%s/%s'):format(h:sub(1, 2), h:sub(3, 4), h)
+    local url = 'http://blzddist1-a.akamaihd.net/tpr/wow/data/' .. suffix
+    local sink = {}
+    local ok, status = http.request({
+      headers = {
+        Range = 'bytes=' .. v.rstart .. '-' .. v.rend,
+      },
+      sink = ltn12.sink.table(sink),
+      url = url,
+    })
+    assert(ok, url)
+    assert(status == 206, status .. ' ' .. url)
+    return table.concat(sink)
   end
 end)()
-
-local processFile = (function()
-  local lxp = require('lxp')
-  local function doProcessFile(fn, root)
-    local content = handle:readFile(fn)
-    if not content then
-      return false
-    end
-    save(fn, content)
-    if fn:sub(-4) == '.xml' then
-      local parser = lxp.new({
-        StartElement = function(_, name, attrs)
-          local lname = string.lower(name)
-          if (lname == 'include' or lname == 'script') and attrs.file then
-            if doProcessFile(joinRelative(fn, attrs.file), root) then
-              return true
-            end
-            return root and doProcessFile(normalizePath(path.join(root, attrs.file)), root)
-          end
-        end,
-      })
-      parser:parse(content)
-      parser:close()
-    end
-    return true
-  end
-  return doProcessFile
-end)()
-
-local function processTocDir(dir)
-  local addonName = path.basename(dir)
-  local tocName = path.join(dir, addonName .. '_' .. build.flavor .. '.toc')
-  local tocContent = handle:readFile(tocName)
-  if not tocContent then
-    tocName = path.join(dir, addonName .. '.toc')
-    tocContent = handle:readFile(tocName)
-  end
-  if tocContent then
-    save(tocName, tocContent)
-    for line in tocContent:gmatch('[^\r\n]+') do
-      if line:sub(1, 1) ~= '#' then
-        processFile(joinRelative(tocName, line:gsub('%s*$', '')), dir)
-      end
-    end
-  end
-  local bindingsName = path.join(dir, 'Bindings.xml')
-  local bindingsContent = handle:readFile(bindingsName)
-  if not bindingsContent then
-    bindingsName = bindingsName:gsub('^Interface/', 'Interface_' .. build.flavor .. '/')
-    bindingsContent = handle:readFile(bindingsName)
-  end
-  if bindingsContent then
-    save(bindingsName, bindingsContent)
-  end
+for k, v in require('pl.tablex').sort(cmap) do
+  log('writing', k)
+  path.mkdir(path.dirname(path.join(outdir, k)))
+  require('pl.file').write(path.join(outdir, k), fetch(v))
 end
-
-for _, db in ipairs(dofile('build/products/' .. product .. '/dblist.lua')) do
-  save(path.join('db2', db .. '.db2'), handle:readFile(fdids[db:lower()]))
-end
-
-processTocDir('Interface/FrameXML')
-do
-  -- Yes, ManifestInterfaceTOCData fdid and sig are hardcoded.
-  local tocdata = handle:readFile(1267335)
-  for _, filepath in require('tools.dbc').rows(tocdata, 's') do
-    processTocDir(normalizePath(filepath))
-  end
-  processTocDir('Interface/AddOns/Blizzard_APIDocumentationGenerated')
-end
-processFile('Interface/FrameXML/UI.xsd')
-processFile('Interface/FrameXML/UI_Shared.xsd')
