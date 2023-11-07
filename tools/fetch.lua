ffad1a051b583e94f2b54181e99fc986b169451a
Ferronn <ferronn@ferronn.dev>
2023-11-07 18:58:00 +0000

    use casc blte

--- tools/fetch.lua
+++ tools/fetch.lua
@@ -1,3 +1,7 @@
+-- Don't let casc use any system backdoors.
+os.execute = function(...) -- luacheck: ignore
+  error('attempt to call execute(' .. table.concat({ ... }) .. ')')
+end
 local args = (function()
   local parser = require('argparse')()
   parser:argument('product', 'product to fetch')
@@ -14,23 +18,25 @@ if path.isdir(outdir) then
   require('pl.dir').rmtree(outdir)
 end
 local fetch = (function()
-  local http = require('socket.http')
-  local ltn12 = require('ltn12')
+  local http = require('socket.http').request
+  local tsink = require('ltn12').sink.table
+  local blte = require('casc.blte').readData
+  local opts = { zerofillEncryptedChunks = true }
   return function(v)
     local h = v.archive
     local suffix = ('%s/%s/%s'):format(h:sub(1, 2), h:sub(3, 4), h)
     local url = 'http://blzddist1-a.akamaihd.net/tpr/wow/data/' .. suffix
     local sink = {}
-    local ok, status = http.request({
+    local ok, status = http({
       headers = {
         Range = 'bytes=' .. v.rstart .. '-' .. v.rend,
       },
-      sink = ltn12.sink.table(sink),
+      sink = tsink(sink),
       url = url,
     })
     assert(ok, url)
     assert(status == 206, status .. ' ' .. url)
-    return table.concat(sink)
+    return (blte(table.concat(sink), nil, opts))
   end
 end)()
 for k, v in require('pl.tablex').sort(cmap) do
