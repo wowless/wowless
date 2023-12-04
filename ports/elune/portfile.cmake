2f9f2e7dc87d8193bd6b74dcc5f63e58e556162f
Ferronn <ferronn@ferronn.dev>
2023-12-04 20:29:07 +0000

    progress, maybe

--- ports/elune/portfile.cmake
+++ ports/elune/portfile.cmake
@@ -3,6 +3,7 @@ vcpkg_from_github(
   REPO meorawr/elune
   REF 7fc7271922fbc0a371ef15d699ffa0b2be649a81
   SHA512 3dade88ec43955ec0be1901d39f41b7f7e65aecc5e654c0efab97cdd3503a75ebece8f7d5eef3ab687d3bb319c71260a25dbca87079f3694ae50339f1a6e514e
+  HEAD_REF main
 )
 vcpkg_cmake_configure(
   SOURCE_PATH "${SOURCE_PATH}"
@@ -12,5 +13,7 @@ vcpkg_cmake_configure(
 )
 vcpkg_cmake_install()
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
-#vcpkg_cmake_config_fixup()
+file(RENAME "${CURRENT_PACKAGES_DIR}/lib/cmake/${PORT}" "${CURRENT_PACKAGES_DIR}/share/${PORT}/cmake")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/lib/cmake")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib/cmake")
