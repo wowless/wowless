2cb8f6594941ad845b05594ca66ef8ced905b64a
Ferronn <ferronn@ferronn.dev>
2023-12-08 17:33:50 +0000

    rump sql, send event

--- data/impl/DoEmote.lua
+++ data/impl/DoEmote.lua
@@ -1,3 +1,8 @@
-local api, token, user, hold = ...
-api.log(1, 'DoEmote(%s, %s, %s) called', token, tostring(user), tostring(hold))
+local api, sql, token, user, hold = ...
+local text = sql(token)
+if text then
+  api.SendEvent('CHAT_MSG_TEXT_EMOTE', text, '', '', '', '', '', 0, 0, '', 0, 0)
+else
+  api.log(1, 'DoEmote(%s) called', token, tostring(user), tostring(hold))
+end
 return false
