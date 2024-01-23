ea9f8d650812aa8618d24c9b016724c5e97018f4
Ferronn <ferronn@ferronn.dev>
2024-01-23 16:20:40 -0600

    retire dbc

--- tools/db2dump.lua
+++ tools/db2dump.lua
@@ -1,14 +1,14 @@
 local args = (function()
   local parser = require('argparse')()
-  parser:flag('--dbc -d', 'use dbc instead of db2')
   parser:argument('product', 'product')
   parser:argument('db2', 'db2')
   return parser:parse()
 end)()
-local rows = require(args.dbc and 'tools.dbc' or 'tools.db2').rows
+local rows = require('tools.db2').rows
 local dbdefs = dofile('build/products/' .. args.product .. '/dbdefs.lua')
 local db2file = 'extracts/' .. args.product .. '/db2/' .. args.db2 .. '.db2'
 local content = assert(require('pl.file').read(db2file))
-for row in rows(content, dbdefs[args.db2]) do
-  print(row[0], unpack(row))
+local dbdef = dbdefs[args.db2]
+for row in rows(content, dbdef) do
+  print(unpack(row, 1, #dbdef))
 end
