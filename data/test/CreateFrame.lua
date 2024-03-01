6c61bd7faff7ca345e5117b46dea06c229ac8fda
Ferronn <ferronn@ferronn.dev>
2024-03-01 08:16:56 -0600

    CreateFrame test: PingPin stuff

--- data/test/CreateFrame.lua
+++ data/test/CreateFrame.lua
@@ -1,6 +1,17 @@
 local t = ...
 return {
-  NonsenseType = function()
-    assert(not pcall(t.env.CreateFrame, 'NonsenseType'))
+  failures = function()
+    local types = {
+      'NonsenseType',
+      'PingPin',
+      'PingPinFrame',
+    }
+    local tt = {}
+    for _, v in ipairs(types) do
+      tt[v] = function()
+        assert(not pcall(t.env.CreateFrame, v))
+      end
+    end
+    return tt
   end,
 }
