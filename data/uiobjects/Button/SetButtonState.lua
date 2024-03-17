3f5dd2d556f9164134ba5b4b6623be5544678274
Ferronn <ferronn@ferronn.dev>
2024-03-17 18:07:01 -0500

    +SimpleButtonStateToken stringenum

--- data/uiobjects/Button/SetButtonState.lua
+++ data/uiobjects/Button/SetButtonState.lua
@@ -2,13 +2,9 @@ local function setShown(t, v)
   return t and t:SetShown(v)
 end
 return (function(self, state, locked)
-  local d = state == 'DISABLED'
-  local n = state == 'NORMAL'
-  local p = state == 'PUSHED'
-  assert(d or n or p, 'invalid state')
   self.buttonState = state
   self.buttonLocked = locked
-  setShown(self.disabledTexture, d)
-  setShown(self.normalTexture, n)
-  setShown(self.pushedTexture, p)
+  setShown(self.disabledTexture, state == 'DISABLED')
+  setShown(self.normalTexture, state == 'NORMAL')
+  setShown(self.pushedTexture, state == 'PUSHED')
 end)(...)
