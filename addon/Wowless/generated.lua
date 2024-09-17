c938947e68f715c7036f95c4ce4067e28db5e262
Ferronn <ferronn@ferronn.dev>
2024-09-17 16:27:24 -0500

    limit string->uiobject coercion to fonts

--- addon/Wowless/generated.lua
+++ addon/Wowless/generated.lua
@@ -266,6 +266,7 @@ G.testsuite.generated = function()
     local arg = {
       assertEquals = G.assertEquals,
       assertRecursivelyEqual = G.assertRecursivelyEqual,
+      check1 = G.check1,
       check2 = G.check2,
       check3 = G.check3,
       check6 = G.check6,
