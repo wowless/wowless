fc08351e455f2da8cc48b6c91030e7ae92dff586
Ferronn <ferronn@ferronn.dev>
2024-09-18 06:43:53 -0500

    mainline GetClassInfo doesn't use >=

--- data/test/GetClassInfo_Classic.lua
+++ data/test/GetClassInfo_Classic.lua
@@ -2,7 +2,7 @@ local T = ...
 return T.wowless and T.wowless.lite and {}
   or {
     hugeneg = function()
-      T.retn(0, T.env.GetClassInfo(10000))
+      T.check3('Warrior', 'WARRIOR', 1, T.env.GetClassInfo(-10000))
     end,
     hugepos = function()
       T.retn(0, T.env.GetClassInfo(10000))
