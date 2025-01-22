0bb00386b9a334e076db059149139680ca72e3a0
Ferronn <ferronn@ferronn.dev>
2025-01-22 21:28:25 +0000

    prototypes for scrollframe impls

--- data/uiobjects/ScrollFrame/SetScrollChild.lua
+++ data/uiobjects/ScrollFrame/SetScrollChild.lua
@@ -1,16 +1,9 @@
-return (function(self, ...)
-  assert(select('#', ...) == 1, 'SetScrollChild: wrong number of arguments')
-  local scrollChild = ...
-  assert(type(scrollChild) == 'table', 'Usage: self:SetScrollChild(scrollChild)')
-  if type(scrollChild) == 'string' then
-    scrollChild = api.env[scrollChild]
-  end
-  local old = self.scrollChild
-  if old then
-    api.SetParent(old, nil)
-  end
-  self.scrollChild = scrollChild and api.UserData(scrollChild)
-  if self.scrollChild then
-    api.SetParent(self.scrollChild, self)
-  end
-end)(...)
+local self, scrollChild = ...
+local old = self.scrollChild
+if old then
+  api.SetParent(old, nil)
+end
+self.scrollChild = scrollChild
+if self.scrollChild then
+  api.SetParent(self.scrollChild, self)
+end
