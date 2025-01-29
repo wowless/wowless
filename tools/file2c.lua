7be825e7c5277eb82a4aa250bb95de6336903472
Ferronn <ferronn@ferronn.dev>
2025-01-29 19:22:36 +0000

    file2c: export a module struct

--- tools/file2c.lua
+++ tools/file2c.lua
@@ -7,11 +7,28 @@ end
 local package, file, output = arg[1], arg[2], arg[3]
 local input = readfile(file)
 io.output(output)
-io.write(('const char lua2c_%s[] = {'):format(package:gsub('%.', '_')))
+io.write('static const char code[] = {')
 for i = 1, input:len() do
   if i % 12 == 1 then
     io.write('\n ')
   end
   io.write((' 0x%02x,'):format(input:byte(i)))
 end
-io.write('\n};\n')
+io.write([[
+
+};
+struct module {
+  const char *name;
+  const char *code;
+  int size;
+  const char *file;
+};
+]])
+io.write(
+  ('const struct module lua2c_%s = { "%s", code, %d, "@%s" };\n'):format(
+    package:gsub('%.', '_'),
+    package,
+    input:len(),
+    file
+  )
+)
