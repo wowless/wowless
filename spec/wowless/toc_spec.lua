73e21c067a25b8673f36e0c709c28b325a67ff8b
Ferronn <ferronn@ferronn.dev>
2025-03-25 17:21:17 -0500

    resolve test

--- spec/wowless/toc_spec.lua
+++ spec/wowless/toc_spec.lua
@@ -1,4 +1,16 @@
-describe('toc', function()
-  local toc = require('wowless.toc')
-  assert(toc ~= nil)
+describe('wowless.toc', function()
+  local wowlesstoc = require('wowless.toc')
+  for flavor in pairs(require('runtime.flavors')) do
+    describe(flavor, function()
+      local tocutil = wowlesstoc(flavor)
+      describe('resolve', function()
+        local resolve = tocutil.resolve
+        it('wowless addon', function()
+          local file, content = resolve('addon/Wowless')
+          assert.same('addon/Wowless/Wowless.toc', file)
+          assert.Not.Nil(content)
+        end)
+      end)
+    end)
+  end
 end)
