039653686bc33f39ba4673fa0b40eaebb64c2188
Ferronn <ferronn@ferronn.dev>
2025-03-26 21:58:00 -0500

    toc parse

--- wowless/toc.lua
+++ wowless/toc.lua
@@ -17,7 +17,25 @@ for k, v in pairs(flavors) do
 end
 
 return function(flavor)
+  local function parse(content)
+    local attrs = {}
+    local files = {}
+    for line in content:gmatch('[^\r\n]+') do
+      line = line:match('^%s*(.-)%s*$')
+      if line:sub(1, 3) == '## ' then
+        local key, value = line:match('([^:]+): (.*)', 4)
+        if key then
+          attrs[key] = value
+        end
+      elseif line ~= '' and line:sub(1, 1) ~= '#' then
+        table.insert(files, line)
+      end
+    end
+    return { attrs = attrs, files = files }
+  end
+
   return {
+    parse = parse,
     suffixes = suffixes[flavor],
   }
 end
