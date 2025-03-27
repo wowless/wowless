7a22f246d80d97b27dfee298d508ba679739f8ab
Ferronn <ferronn@ferronn.dev>
2025-03-27 17:24:56 -0500

    toc: shared parse function

--- wowless/toc.lua
+++ wowless/toc.lua
@@ -16,6 +16,24 @@ for k, v in pairs(flavors) do
   suffixes[k] = t
 end
 
+local function parse(content)
+  local attrs = {}
+  local files = {}
+  for line in content:gmatch('[^\r\n]+') do
+    line = line:match('^%s*(.-)%s*$')
+    if line:sub(1, 3) == '## ' then
+      local key, value = line:match('([^:]+): (.*)', 4)
+      if key then
+        attrs[key] = value
+      end
+    elseif line ~= '' and line:sub(1, 1) ~= '#' then
+      table.insert(files, line)
+    end
+  end
+  return attrs, files
+end
+
 return {
+  parse = parse,
   suffixes = suffixes,
 }
