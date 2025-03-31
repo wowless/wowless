2a21dc9e948c2983cd69ddb6f263d99a0adc732f
Ferronn <ferronn@ferronn.dev>
2025-03-31 08:28:18 -0500

    GetGlobalEnvironment and IsInGlobalEnvironment

--- wowless/modules/env.lua
+++ wowless/modules/env.lua
@@ -1,8 +1,16 @@
-return function()
+return function(api)
+  local env = api.env
   return {
     GetCurrentEnvironment = function()
       -- getfenv(2) but accounting for the api loading stack
       return getfenv(5)
+    end,
+    GetGlobalEnvironment = function()
+      return env
+    end,
+    IsInGlobalEnvironment = function()
+      -- getfenv(2) but accounting for the api loading stack
+      return getfenv(5) == env
     end,
   }
 end
