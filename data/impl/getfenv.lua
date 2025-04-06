d51dccff68ba94742e471e9b552efc01f9994e1b
Ferronn <ferronn@ferronn.dev>
2025-04-05 20:03:47 -0500

    env index fixes

--- data/impl/getfenv.lua
+++ data/impl/getfenv.lua
@@ -1,4 +1,4 @@
 local env, arg = ...
 local narg = tonumber(arg)
-local fenv = getfenv(narg and narg + 3 or arg)
+local fenv = getfenv(narg and narg + 4 or arg)
 return fenv == _G and env or fenv
--- wowless/modules/env.lua
+++ wowless/modules/env.lua
@@ -3,18 +3,18 @@ return function(api)
   return {
     GetCurrentEnvironment = function()
       -- getfenv(2) but accounting for the api loading stack
-      return getfenv(5)
+      return getfenv(6)
     end,
     GetGlobalEnvironment = function()
       return env
     end,
     IsInGlobalEnvironment = function()
       -- getfenv(2) but accounting for the api loading stack
-      return getfenv(5) == env
+      return getfenv(6) == env
     end,
     SwapToGlobalEnvironment = function()
       -- setfenv(2, env) but accounting for the api loading stack
-      setfenv(5, env)
+      setfenv(6, env)
     end,
   }
 end
