9ac58b685e81dac313468860f4d3456a9c134c71
Ferronn <ferronn@ferronn.dev>
2025-04-17 10:33:05 -0500

    datetime module

--- data/impl/C_DateAndTime.AdjustTimeByMinutes.lua
+++
@@ -1,5 +0,0 @@
-local datalua, calendarTime, minutes = ...
-local util = require('wowless.util')
-local date = util.calendarTimeToDate(calendarTime)
-assert(date:addminutes(minutes))
-return util.dateToCalendarTime(datalua, date)
