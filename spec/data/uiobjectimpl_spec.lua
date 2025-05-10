830bfa7b9eddddb554d04d542cb4b0bdbec351ba
Ferronn <ferronn@ferronn.dev>
2025-05-10 13:08:50 -0400

    uiobjectimpl load test

--- spec/data/uiobjectimpl_spec.lua
+++ spec/data/uiobjectimpl_spec.lua
@@ -1,13 +1,24 @@
 describe('uiobjectimpl', function()
+  local luas = {}
+  for _, f in ipairs(require('pl.dir').getallfiles('data/uiobjects')) do
+    luas[f] = assert(require('pl.file').read(f))
+  end
   it('has the right files', function()
     local expected = {}
     for k in pairs(require('build.data.uiobjectimpl')) do
       expected['data/uiobjects/' .. k .. '.lua'] = true
     end
     local actual = {}
-    for _, f in ipairs(require('pl.dir').getallfiles('data/uiobjects')) do
+    for f in pairs(luas) do
       actual[f] = true
     end
     assert.same(expected, actual)
   end)
+  for filename, content in pairs(luas) do
+    describe(filename, function()
+      it('loads', function()
+        assert(loadstring(content, '@' .. filename))
+      end)
+    end)
+  end
 end)
