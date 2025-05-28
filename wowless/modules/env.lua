fbc029a104e628417a605dd3acc6fb2a3a873c6d
Ferronn <ferronn@ferronn.dev>
2025-05-28 10:34:48 -0500

    bubblewrap in C, saves 3% time

--- wowless/modules/env.lua
+++ wowless/modules/env.lua
@@ -3,23 +3,23 @@ return function(api)
   return {
     getfenv = function(arg)
       local narg = tonumber(arg)
-      local fenv = getfenv(narg and narg + 6 or arg)
+      local fenv = getfenv(narg and narg + 5 or arg)
       return fenv == _G and env or fenv
     end,
     GetCurrentEnvironment = function()
       -- getfenv(2) but accounting for the api loading stack
-      return getfenv(7)
+      return getfenv(6)
     end,
     GetGlobalEnvironment = function()
       return env
     end,
     IsInGlobalEnvironment = function()
       -- getfenv(2) but accounting for the api loading stack
-      return getfenv(7) == env
+      return getfenv(6) == env
     end,
     SwapToGlobalEnvironment = function()
       -- setfenv(2, env) but accounting for the api loading stack
-      setfenv(7, env)
+      setfenv(6, env)
     end,
   }
 end
