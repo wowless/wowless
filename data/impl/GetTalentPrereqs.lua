33881cb56dbe265f035459077298edcc060f02bd
Ferronn <ferronn@ferronn.dev>
2025-07-01 22:49:57 -0400

    fix up GetTalentPrereqs

--- data/impl/GetTalentPrereqs.lua
+++ data/impl/GetTalentPrereqs.lua
@@ -5,7 +5,7 @@ return function(tabIndex, talentIndex)
   for tier, column in sql(api.modules.units.player.class, tabIndex, talentIndex) do
     t[num + 1] = tier
     t[num + 2] = column
-    num = num + 3
+    num = num + 4
   end
   return unpack(t, 1, num)
 end
