5c4cf11d2211ea78446b1831fb34bfbdaa7e05cc
Ferronn <ferronn@ferronn.dev>
2025-08-05 16:49:28 +0000

    auxsort

--- wowapi/cyaml.c
+++ wowapi/cyaml.c
@@ -2,6 +2,102 @@
 #include "lua.h"
 #include "yaml.h"
 
+/** START adapted from ltablib.c from elune. */
+
+static void set2(lua_State *L, int idx, int i, int j) {
+  lua_rawseti(L, idx, i);
+  lua_rawseti(L, idx, j);
+}
+
+static int sort_comp(lua_State *L, int a, int b) {
+  size_t na;
+  size_t nb;
+  const char *sa = lua_tolstring(L, a, &na);
+  const char *sb = lua_tolstring(L, b, &nb);
+  int cmp = strncasecmp(sa, sb, na < nb ? na : nb);
+  return cmp < 0 || cmp == 0 && na < nb;
+}
+
+static void auxsort(lua_State *L, int idx, int l, int u) {
+  while (l < u) { /* for tail recursion */
+    int i;
+    int j;
+    /* sort elements a[l], a[(l+u)/2] and a[u] */
+    lua_rawgeti(L, idx, l);
+    lua_rawgeti(L, idx, u);
+    if (sort_comp(L, -1, -2)) { /* a[u] < a[l]? */
+      set2(L, idx, l, u);       /* swap a[l] - a[u] */
+    } else {
+      lua_pop(L, 2);
+    }
+    if (u - l == 1) {
+      break; /* only 2 elements */
+    }
+    i = (l + u) / 2;
+    lua_rawgeti(L, idx, i);
+    lua_rawgeti(L, idx, l);
+    if (sort_comp(L, -2, -1)) { /* a[i]<a[l]? */
+      set2(L, idx, i, l);
+    } else {
+      lua_pop(L, 1); /* remove a[l] */
+      lua_rawgeti(L, idx, u);
+      if (sort_comp(L, -1, -2)) { /* a[u]<a[i]? */
+        set2(L, idx, i, u);
+      } else {
+        lua_pop(L, 2);
+      }
+    }
+    if (u - l == 2) {
+      break; /* only 3 elements */
+    }
+    lua_rawgeti(L, idx, i); /* Pivot */
+    lua_pushvalue(L, -1);
+    lua_rawgeti(L, idx, u - 1);
+    set2(L, idx, i, u - 1);
+    /* a[l] <= P == a[u-1] <= a[u], only need to sort from l+1 to u-2 */
+    i = l;
+    j = u - 1;
+    for (;;) { /* invariant: a[l..i] <= P <= a[j..u] */
+      /* repeat ++i until a[i] >= P */
+      while (lua_rawgeti(L, idx, ++i), sort_comp(L, -1, -2)) {
+        if (i > u) {
+          luaL_error(L, "invalid order function for sorting");
+        }
+        lua_pop(L, 1); /* remove a[i] */
+      }
+      /* repeat --j until a[j] <= P */
+      while (lua_rawgeti(L, idx, --j), sort_comp(L, -3, -1)) {
+        if (j < l) {
+          luaL_error(L, "invalid order function for sorting");
+        }
+        lua_pop(L, 1); /* remove a[j] */
+      }
+      if (j < i) {
+        lua_pop(L, 3); /* pop pivot, a[i], a[j] */
+        break;
+      }
+      set2(L, idx, i, j);
+    }
+    lua_rawgeti(L, idx, u - 1);
+    lua_rawgeti(L, idx, i);
+    set2(L, idx, u - 1, i); /* swap pivot (a[u-1]) with a[i] */
+    /* a[l..i-1] <= a[i] == P <= a[i+1..u] */
+    /* adjust so that smaller half is in [j..i] and larger one in [l..u] */
+    if (i - l < u - i) {
+      j = l;
+      i = i - 1;
+      l = i + 2;
+    } else {
+      j = i + 1;
+      i = u;
+      u = j - 2;
+    }
+    auxsort(L, idx, j, i); /* call recursively the smaller one */
+  } /* repeat the routine for the larger one */
+}
+
+/** END adapted from ltablib.c from elune. */
+
 static void x(lua_State *L, int err) {
   if (err == 0) {
     luaL_error(L, "yaml: internal error");
@@ -208,9 +304,11 @@ static void printvalue(lua_State *L, yaml_emitter_t *emitter,
       } else {
         x(L, yaml_mapping_start_event_initialize(event, 0, 0, 1, 0));
         x(L, yaml_emitter_emit(emitter, event));
-        lua_pushnil(L);
-        while (lua_next(L, -3)) {
-          printscalar(L, emitter, event, -2);
+        auxsort(L, lua_gettop(L), 1, lua_objlen(L, -1));
+        for (int i = 1; i <= n; ++i) {
+          lua_rawgeti(L, -1, i);
+          printscalar(L, emitter, event, -1);
+          lua_rawget(L, -3);
           printvalue(L, emitter, event);
           lua_pop(L, 1);
         }
