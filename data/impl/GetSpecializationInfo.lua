4e5ae2df9deac948c2d49606d686221a8997c00b
Ferronn <ferronn@ferronn.dev>
2025-08-25 10:40:01 -0500

    bump wowt to 11.2.5.62687

--- data/impl/GetSpecializationInfo.lua
+++
@@ -1,6 +0,0 @@
--- TODO support remaining arguments
-local units, sql = ...
-return function(specIndex)
-  local player = units.player
-  return sql(player.class, player.sex, specIndex)
-end
--- wowless/modules/addons.lua
+++ wowless/modules/addons.lua
@@ -1,13 +1,23 @@
-return function()
+return function(datalua)
   local addons = {}
+  local config = datalua.config.modules and datalua.config.modules.addons or {}
+
+  local getAddOnInfoUpdateAvailable
+  if config.GetAddOnInfo_noUpdateAvailable then
+    getAddOnInfoUpdateAvailable = function() end
+  else
+    getAddOnInfoUpdateAvailable = function()
+      return false
+    end
+  end
 
   local function GetAddOnInfo(addon)
     if not addon then
-      return 'FIXME', nil, nil, false, 'MISSING', 'INSECURE', false
+      return 'FIXME', nil, nil, false, 'MISSING', 'INSECURE', getAddOnInfoUpdateAvailable()
     else
       local name = addon.name
       local secure = addon.name:sub(1, 9) == 'Blizzard_' and 'SECURE' or 'INSECURE'
-      return name, addon.attrs.Title or '', addon.attrs.Notes or '', true, '', secure, false
+      return name, addon.attrs.Title or '', addon.attrs.Notes or '', true, '', secure, getAddOnInfoUpdateAvailable()
     end
   end
 
