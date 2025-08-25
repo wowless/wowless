7a94843584303b35a96552aa81dfed241cbd5377
Ferronn <ferronn@ferronn.dev>
2025-08-25 10:53:14 -0500

    clean up config_spec a bit

--- spec/data/config_spec.lua
+++ spec/data/config_spec.lua
@@ -3,7 +3,6 @@ describe('config', function()
     describe(p, function()
       local config = require('build.data.products.' .. p .. '.config')
       local apis = require('build.data.products.' .. p .. '.apis')
-      local globals = require('build.data.products.' .. p .. '.globals')
       local ns = {}
       for k in pairs(apis) do
         local dot = k:find('%.')
@@ -19,37 +18,6 @@ describe('config', function()
             for k in pairs(capsulecfg.apinamespaces or {}) do
               it(k, function()
                 assert(ns[k])
-              end)
-            end
-          end)
-          describe('enums', function()
-            local enums = globals.Enum or {}
-            for k in pairs(capsulecfg.enums or {}) do
-              it(k, function()
-                assert(enums[k])
-              end)
-            end
-          end)
-        end)
-      end)
-      describe('docs', function()
-        local doccfg = config.docs or {}
-        describe('apis', function()
-          local apicfg = doccfg.apis or {}
-          describe('skip_namespaces', function()
-            local skipcfg = apicfg.skip_namespaces or {}
-            local allns = {}
-            for k in pairs(apis) do
-              local dotpos = k:find('%.')
-              if dotpos then
-                allns[k:sub(1, dotpos - 1)] = true
-              end
-            end
-            for k in pairs(skipcfg) do
-              describe(k, function()
-                it('must be unused', function()
-                  assert.Nil(allns[k])
-                end)
               end)
             end
           end)
