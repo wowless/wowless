a91c527a217362b180059c477e03224267ea0eee
Ferronn <ferronn@ferronn.dev>
2025-08-27 15:38:45 -0500

    generate sqlite3 databases from cmake (#457)

--- wowless/modules/sqlitedb.lua
+++ wowless/modules/sqlitedb.lua
@@ -1,5 +1,5 @@
 return function(datalua, loadercfg)
   local base = loadercfg.rootDir and 'data' or 'schema'
-  local file = ('build/products/%s/%s.sqlite3'):format(datalua.product, base)
+  local file = ('build/cmake/%s_%s.sqlite3'):format(datalua.product, base)
   return require('wowless.sqlite').open(file)
 end
