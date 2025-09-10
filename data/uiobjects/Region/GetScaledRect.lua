a1422cc41e155693f9920e78b16d9603f541d78c
Ferronn <ferronn@ferronn.dev>
2025-09-10 13:28:08 -0500

    fix left and bottom return order on GetRect

--- data/uiobjects/Region/GetScaledRect.lua
+++ data/uiobjects/Region/GetScaledRect.lua
@@ -1,5 +1,5 @@
 return function(self)
   local s = self:GetEffectiveScale()
-  local b, l, w, h = self:GetRect()
-  return b * s, l * s, w * s, h * s
+  local l, b, w, h = self:GetRect()
+  return l * s, b * s, w * s, h * s
 end
