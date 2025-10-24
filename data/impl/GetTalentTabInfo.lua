9bf693d40327e0549b434d9aa4ab56b96593a119
Ferronn <ferronn@ferronn.dev>
2025-10-24 10:06:52 -0500

    bump wow_classic_era to 1.15.8.63829

--- data/impl/GetTalentTabInfo.lua
+++
@@ -1,5 +0,0 @@
-local units, sql = ...
-return function(tabIndex) -- TODO honor isInspect
-  local name, filename = sql(units.player.class, tabIndex)
-  return nil, name, nil, nil, 0, filename, 0, false
-end
