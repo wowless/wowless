ca241bec0ecd26c88b7b0d96f302dbf8a34655e4
Ferronn <ferronn@ferronn.dev>
2026-01-16 14:42:38 -0600

    pre-commit autoupdate

--- data/sql/C_Traits.GetNodeInfo.conditions.sql
+++ data/sql/C_Traits.GetNodeInfo.conditions.sql
@@ -6,8 +6,8 @@ SELECT
 FROM TraitNode AS node
 LEFT JOIN TraitNodeXTraitCond AS nxc ON node.ID = nxc.TraitNodeID
 LEFT JOIN TraitNodeGroupXTraitNode AS gxn ON node.ID = gxn.TraitNodeID
-LEFT JOIN TraitNodeGroup AS `group` ON `group`.ID = gxn.TraitNodeGroupID
-LEFT JOIN TraitNodeGroupXTraitCond AS gxc ON gxc.TraitNodeGroupID = `group`.ID
+LEFT JOIN TraitNodeGroup AS `group` ON gxn.TraitNodeGroupID = `group`.ID
+LEFT JOIN TraitNodeGroupXTraitCond AS gxc ON `group`.ID = gxc.TraitNodeGroupID
 LEFT JOIN TraitCond AS cond1 ON gxc.TraitCondID = cond1.ID
 LEFT JOIN TraitCond AS cond2 ON nxc.TraitCondID = cond2.ID
 LEFT JOIN SpecSetMember AS specset1 ON cond1.SpecSetID = specset1.SpecSet
