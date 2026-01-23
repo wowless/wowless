f4663aa06f57b87b7b3daec35414454a5b77a509
Ferronn <ferronn@ferronn.dev>
2026-01-23 15:43:41 -0600

    THETAINT, fixes #431

--- data/uiobjects/Frame/GetAttribute.lua
+++ data/uiobjects/Frame/GetAttribute.lua
@@ -1,10 +1,10 @@
-local err = 'Arguments: ("name")\nLua Taint: Wowless' -- issue #431
 return function(self, arg1, arg2, ...)
   local attrs = self.attributes
   if arg1 and not arg2 then
     return attrs[arg1]
   elseif not arg2 or select('#', ...) == 0 then
-    error(err, 0)
+    local taint = _G.THETAINT and '\nLua Taint: ' .. _G.THETAINT or ''
+    error('Arguments: ("name")' .. taint, 0)
   else
     arg1 = arg1 or ''
     local arg3 = (...) or ''
