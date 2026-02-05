b9b959c6581ac1aa0fabd3b2164836cd3d7275a7
Ferronn <ferronn@ferronn.dev>
2026-02-05 09:46:18 -0600

    SetScrollChild manipulates points

--- data/uiobjects/ScrollFrame/SetScrollChild.lua
+++ data/uiobjects/ScrollFrame/SetScrollChild.lua
@@ -1,11 +1,13 @@
-local api = ...
+local api, points = ...
 return function(self, scrollChild)
   local old = self.scrollChild
   if old then
     api.SetParent(old, nil)
   end
   self.scrollChild = scrollChild
-  if self.scrollChild then
-    api.SetParent(self.scrollChild, self)
+  if scrollChild then
+    api.SetParent(scrollChild, self)
+    points.ClearAllPoints(scrollChild)
+    points.SetPointInternal(scrollChild, 'TOPLEFT', self, 'TOPLEFT', 0, 0)
   end
 end
