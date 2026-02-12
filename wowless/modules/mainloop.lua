df9f0e94b27cd503554ffa14805703e8dd8122bd
Ferronn <ferronn@ferronn.dev>
2026-02-12 14:08:27 -0600

    mainloop: use region.GetRect instead of index method

--- wowless/modules/mainloop.lua
+++ wowless/modules/mainloop.lua
@@ -1,6 +1,7 @@
-return function(api, scripts, time, visibility)
+return function(api, region, scripts, time, visibility)
   local Advance = time.Advance
   local frames = api.frames
+  local GetRect = region.GetRect
   local IsVisible = visibility.IsVisible
   local RunScript = scripts.RunScript
 
@@ -13,7 +14,7 @@ return function(api, scripts, time, visibility)
     end
     for frame in frames:entries() do
       if IsVisible(frame) then
-        frame:GetRect() -- force recomputation if dirty
+        GetRect(frame) -- force recomputation if dirty
       end
     end
   end
