2404e86720c42858ac1af0a0f56577ee081c0000
Ferronn <ferronn@ferronn.dev>
2026-03-13 09:43:31 -0500

    addons: inline GetAddOnInfo_noUpdateAvailable behavior
    The flag was true for all products, so remove it from config and schema
    and unconditionally omit the update-available return value.

--- wowless/modules/addons.lua
+++ wowless/modules/addons.lua
@@ -1,23 +1,12 @@
-return function(datalua)
+return function()
   local addons = {}
-  local config = datalua.config.modules and datalua.config.modules.addons or {}
-
-  local getAddOnInfoUpdateAvailable
-  if config.GetAddOnInfo_noUpdateAvailable then
-    getAddOnInfoUpdateAvailable = function() end
-  else
-    getAddOnInfoUpdateAvailable = function()
-      return false
-    end
-  end
-
   local function GetAddOnInfo(addon)
     if not addon then
-      return 'FIXME', nil, nil, false, 'MISSING', 'INSECURE', getAddOnInfoUpdateAvailable()
+      return 'FIXME', nil, nil, false, 'MISSING', 'INSECURE'
     else
       local name = addon.name
       local secure = addon.name:sub(1, 9) == 'Blizzard_' and 'SECURE' or 'INSECURE'
-      return name, addon.attrs.Title or '', addon.attrs.Notes or '', true, '', secure, getAddOnInfoUpdateAvailable()
+      return name, addon.attrs.Title or '', addon.attrs.Notes or '', true, '', secure
     end
   end
 
