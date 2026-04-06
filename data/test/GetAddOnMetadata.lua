98acf33f05543ed6aee24053bc271bd3226b9f96
Ferronn <ferronn@ferronn.dev>
2026-04-06 12:51:03 -0500

    more precise toc metadata tests #581

--- data/test/GetAddOnMetadata.lua
+++ data/test/GetAddOnMetadata.lua
@@ -1,9 +1,28 @@
 local T, GetAddOnMetadata = ...
-return {
-  missing = function()
-    return T.match(1, nil, GetAddOnMetadata('Wowless', 'WowlessNonsense'))
-  end,
-  present = function()
-    return T.match(1, 'WoW client unit tests', GetAddOnMetadata('Wowless', 'Notes'))
-  end,
+local kv = {
+  Notes = 'WoW client unit tests',
+  Title = 'Wowless',
 }
+local knil = {
+  Dependencies = false,
+  Interface = false,
+  SavedVariables = false,
+  WowlessNonsense = true,
+}
+local tests = {}
+for k, v in pairs(kv) do
+  tests[k] = function()
+    return T.match(1, v, GetAddOnMetadata(T.addonName, k))
+  end
+end
+for k, v in pairs(knil) do
+  assert(not tests[k])
+  tests[k] = function()
+    if v or not T.wowless then
+      return T.match(1, nil, GetAddOnMetadata(T.addonName, k))
+    else
+      assert(T.retn(1, GetAddOnMetadata(T.addonName, k) ~= nil)) -- issue #581
+    end
+  end
+end
+return tests
