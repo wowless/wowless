5a5b8e91e9fc932e0e9a0d6c50c55532abb9ee17
Ferronn <ferronn@ferronn.dev>
2026-06-07 14:34:33 -0500

    gencode: trim unused functionality

--- wowless/modules/gencode.lua
+++ wowless/modules/gencode.lua
@@ -1,21 +1,6 @@
 local hlist = require('wowless.hlist')
 
-return function(api, env, log, luaobjects, typecheck)
-  local function Check(spec, v, isout)
-    local vv, errmsg, iswarn = typecheck(spec, v, isout)
-    if errmsg then
-      local inout = isout and 'output' or 'input'
-      local name = spec.name and (' %q'):format(spec.name) or ''
-      local msg = ('%s%s %s'):format(inout, name, errmsg)
-      if iswarn then
-        log(1, 'warning: ' .. msg)
-      else
-        error(msg, 2)
-      end
-    end
-    return vv
-  end
-
+return function(api)
   local function ToTexture(parent, tex, obj)
     if type(tex) == 'string' or type(tex) == 'number' then
       local t = obj or parent:CreateTexture()
@@ -26,16 +11,9 @@ return function(api, env, log, luaobjects, typecheck)
     end
   end
 
-  local function CreateLuaObject(typename)
-    return luaobjects.CreateProxy(typename, luaobjects.Create(typename))
-  end
-
   return {
-    Check = Check,
-    CreateLuaObject = CreateLuaObject,
     CreateUIObject = api.CreateUIObject,
     hlist = hlist,
-    Mixin = env.mixin,
     SetParent = api.SetParent,
     ToTexture = ToTexture,
   }
