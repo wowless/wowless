c8a3ce9288c6a7505fe8908b2435a00a9fb69489
Ferronn <ferronn@ferronn.dev>
2026-06-30 11:11:16 -0500

    drop unhelpful dotfile support

--- wowless.lua
+++ wowless.lua
@@ -8,7 +8,6 @@ local args = (function()
   run:option('-e --maxerrors', 'quit once this number of errors occur'):convert(tonumber)
   run:option('-s --scripts', 'scripts to execute')
   run:option('-o --output', 'output file')
-  run:option('-d --dotfile', 'write addon dependency graph in graphviz dot format')
   run:flag('--frame0', 'write frame0 debug')
   run:flag('--lite', 'do not load framexml')
   run:flag('--profile', 'dump profile')
@@ -24,7 +23,6 @@ local addonDirs = #args.addondir > 0 and args.addondir
 local runner = require('wowless.runner')
 local modules = runner.run({
   dir = not args.lite and 'build/extracts/' .. args.product or nil,
-  dotfile = args.dotfile,
   frame0 = args.frame0,
   loglevel = args.loglevel,
   maxErrors = args.maxerrors,
