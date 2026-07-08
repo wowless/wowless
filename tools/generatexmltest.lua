a240ba6ac762cab259fa4db7c140a155933eacf5
Ferronn <ferronn@ferronn.dev>
2026-07-08 10:56:22 -0500

    generatedxmltest: generate from lua table

--- tools/generatexmltest.lua
+++ tools/generatexmltest.lua
@@ -15,44 +15,82 @@ local args = (function()
   return parser:parse()
 end)()
 
-local names = {}
-for name in pairs(readyaml('data/scripttypes.yaml')) do
-  table.insert(names, name)
+local sorted = require('pl.tablex').sort
+local scripttypes = readyaml('data/scripttypes.yaml')
+
+local content = {
+  tag = 'Ui',
+  {
+    tag = 'Script',
+    text = 'WowlessLog = {}',
+  },
+  {
+    tag = 'Actor',
+    name = 'WowlessActorTemplate',
+    virtual = true,
+    (function()
+      local scripts = { tag = 'Scripts' }
+      for name in sorted(scripttypes) do
+        table.insert(scripts, {
+          tag = name,
+          text = ([[end, table.insert(WowlessLog, '%s')--]]):format(name),
+        })
+      end
+      return scripts
+    end)(),
+  },
+  {
+    tag = 'Script',
+    text = [[
+      CreateFrame('ModelScene'):CreateActor(nil, 'WowlessActorTemplate')
+      local expected = {}
+      for k, v in _G.Wowless.sorted(_G.WowlessData.UIObjectApis.Actor.scripts) do
+        if v then
+          table.insert(expected, k)
+        else
+          local fmt = 'Frame ModelSceneActor: Unknown script element %s'
+          table.insert(_G.Wowless.ExpectedLuaWarnings, fmt:format(k))
+        end
+      end
+      assertEquals(table.concat(expected, ','), table.concat(WowlessLog, ','))
+    ]],
+  },
+}
+
+local renderXml
+do
+  local function doRenderXml(x, n, t)
+    local tt = { (' '):rep(n), '<', x.tag }
+    local attrs = {}
+    for k, v in pairs(x) do
+      if type(k) == 'string' and k ~= 'tag' and k ~= 'text' then
+        attrs[k] = v
+      end
+    end
+    for k, v in sorted(attrs) do
+      table.insert(tt, (' %s=\'%s\''):format(k, tostring(v)))
+    end
+    table.insert(tt, '>')
+    if x.text then
+      table.insert(tt, x.text)
+      table.insert(tt, '</')
+      table.insert(tt, x.tag)
+      table.insert(tt, '>')
+      table.insert(t, table.concat(tt))
+    else
+      table.insert(t, table.concat(tt))
+      for _, v in ipairs(x) do
+        doRenderXml(v, n + 2, t)
+      end
+      table.insert(t, table.concat({ (' '):rep(n), '</', x.tag, '>' }))
+    end
+  end
+  function renderXml(x)
+    local t = {}
+    doRenderXml(x, 0, t)
+    return table.concat(t, '\n')
+  end
 end
-table.sort(names)
 
-local scripts = {}
-for _, name in ipairs(names) do
-  table.insert(scripts, ('      <%s>end, table.insert(WowlessLog, \'%s\')--</%s>'):format(name, name, name))
-end
-
-local content = table.concat({
-  '<Ui>',
-  '  <!-- Actors support scripts through XML even though they aren\'t ScriptObjects. -->',
-  '  <Script>',
-  '    WowlessLog = {}',
-  '  </Script>',
-  '  <Actor name=\'WowlessActorTemplate\' virtual=\'true\'>',
-  '    <Scripts>',
-  table.concat(scripts, '\n'),
-  '    </Scripts>',
-  '  </Actor>',
-  '  <Script>',
-  '    CreateFrame(\'ModelScene\'):CreateActor(nil, \'WowlessActorTemplate\')',
-  '    local expected = {}',
-  '    for k, v in _G.Wowless.sorted(_G.WowlessData.UIObjectApis.Actor.scripts) do',
-  '      if v then',
-  '        table.insert(expected, k)',
-  '      else',
-  '        local fmt = \'Frame ModelSceneActor: Unknown script element %s\'',
-  '        table.insert(_G.Wowless.ExpectedLuaWarnings, fmt:format(k))',
-  '      end',
-  '    end',
-  '    assertEquals(table.concat(expected, \',\'), table.concat(WowlessLog, \',\'))',
-  '  </Script>',
-  '</Ui>',
-  '',
-}, '\n')
-
-require('pl.file').write(args.output, content)
+require('pl.file').write(args.output, renderXml(content))
 require('tools.util').writedeps(args.output, deps)
