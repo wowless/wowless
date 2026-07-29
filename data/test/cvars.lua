95e7b3178a9263b396ef4813ac4bae8e08a25ade
Ferronn <ferronn@ferronn.dev>
2026-07-28 20:55:30 -0500

    cvars: assert CVAR_UPDATE happens after the update

--- data/test/cvars.lua
+++ data/test/cvars.lua
@@ -1,13 +1,17 @@
 local T, GetCVar, GetCVarBool, SetCVar = ...
-local updates = {}
-local frame = T.env.CreateFrame('Frame')
-frame:RegisterEvent('CVAR_UPDATE')
-frame:SetScript('OnEvent', function(_, _, ...)
-  table.insert(updates, { ... })
-end)
+local function evchecker(v)
+  local updates = {}
+  local frame = T.env.CreateFrame('Frame')
+  frame:RegisterEvent('CVAR_UPDATE')
+  frame:SetScript('OnEvent', function(_, _, ...)
+    table.insert(updates, { GetCVar(v), ... })
+  end)
+  return updates
+end
 return {
   bad = function()
     local v = 'WowlessNonsenseCVar'
+    local updates = evchecker(v)
     T.check1(nil, GetCVar(v))
     T.check1(nil, GetCVarBool(v))
     T.assertEquals(0, #updates)
@@ -18,20 +22,21 @@ return {
   end,
   good = function()
     local v = 'DebugTorsoTwist'
+    local updates = evchecker(v)
     T.check1('0', GetCVar(v))
     T.check1(false, GetCVarBool(v))
     T.assertEquals(0, #updates)
     T.check1(true, SetCVar(v, '1'))
     T.assertEquals(1, #updates)
-    T.check2(v, '1', unpack(updates[1]))
-    updates = {}
+    T.check3('1', v, '1', unpack(updates[1]))
+    updates[1] = nil
     T.check1('1', GetCVar(v))
     T.check1(true, GetCVarBool(v))
     T.check1(true, SetCVar(v, '1'))
     T.assertEquals(0, #updates)
     T.check1(true, SetCVar(v, '0'))
     T.assertEquals(1, #updates)
-    T.check2(v, '0', unpack(updates[1]))
+    T.check3('0', v, '0', unpack(updates[1]))
     T.check1('0', GetCVar(v))
     T.check1(false, GetCVarBool(v))
   end,
