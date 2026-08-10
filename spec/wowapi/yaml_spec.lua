8849eb6be2c6cbe62ad19befe3cbfeb4c8595bca
Ferronn <ferronn@ferronn.dev>
2026-08-10 07:39:33 -0400

    yaml_spec: test round trip

--- spec/wowapi/yaml_spec.lua
+++ spec/wowapi/yaml_spec.lua
@@ -1,36 +1,39 @@
 describe('yaml', function()
   local yaml = require('wowapi.yaml')
+  local ystr = [[
+bool: true
+empty_string: ''
+number: 42
+record:
+  baz: quux
+  frob: nicate
+sequence:
+- 99
+- bar
+string: foo
+]]
+  local ytab = {
+    bool = true,
+    empty_string = '',
+    number = 42,
+    record = { baz = 'quux', frob = 'nicate' },
+    sequence = { 99, 'bar' },
+    string = 'foo',
+  }
   describe('parse', function()
     it('works on empty', function()
       assert.same({}, yaml.parse('\n'))
     end)
     it('works on all types', function()
-      local input = [[
-bool: true
-number: 42
-string: foo
-empty_string: ''
-sequence:
-- 99
-- bar
-record:
-  baz: quux
-  frob: nicate
-]]
-      local expected = {
-        bool = true,
-        number = 42,
-        string = 'foo',
-        empty_string = '',
-        sequence = { 99, 'bar' },
-        record = { baz = 'quux', frob = 'nicate' },
-      }
-      assert.same(expected, yaml.parse(input))
+      assert.same(ytab, yaml.parse(ystr))
     end)
   end)
   describe('pprint', function()
     it('works on empty', function()
       assert.same('\n', yaml.pprint({}))
+    end)
+    it('works on all types', function()
+      assert.same(ystr, yaml.pprint(ytab))
     end)
   end)
 end)
